97478989c440ae60c0cbe5216c55cb056fdd38ac
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-06-14 10:36:49 +0530

    created base org screen

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React from "react";
 import "mdbreact/dist/css/mdb.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
@@ -6,17 +6,20 @@ import {
   BrowserRouter as Router,
   Switch,
   Route,
-  Redirect
-} from 'react-router-dom';
-import Login from './Views/Login/Login'
-import ProfileScreen from './Views/Login/ProfileScreen'
+  Redirect,
+} from "react-router-dom";
+import Login from "./Views/Login/Login";
+import ProfileScreen from "./Views/Login/ProfileScreen";
+import OrganisationScreen from "./Views/Login/OrganisationScreen";
+
 function App() {
   return (
     <React.Fragment>
-      <Router >
+      <Router>
         <Switch>
           <Route exact path="/login" component={Login} />
           <Route exact path="/login/profile" component={ProfileScreen} />
+          <Route exact path="/login/org" component={OrganisationScreen} />
           <Redirect from="/" to="/login" />
         </Switch>
       </Router>
