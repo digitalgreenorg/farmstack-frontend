2a6bf19794d0a36d213035e907f409895955d807
Jayasankar G K <jaykay@Jaykays-MacBook-Pro.local>
2022-07-08 15:04:27 +0530

    policy settings layout test

--- src/Views/Settings/PolicySettings/PolicySettings.js
+++ src/Views/Settings/PolicySettings/PolicySettings.js
@@ -1,6 +1,9 @@
-import React from 'react'
+import React, { useState } from 'react'
 import { Col, Row } from 'react-bootstrap'
 import THEME_COLORS from "../../../Constants/ColorConstants";
+import RichTextEditor from "react-rte";
+import { FileUploader } from "react-drag-drop-files";
+import UploadOrgLogo from "../../../Components/signup/UploadOrgLogo";
 
 const useStyles = {
     btncolor: {
@@ -25,9 +28,43 @@ const useStyles = {
     marginrowtop10px: { "margin-top": "10px" },
     teamword: { "font-weight": 700, "font-size": "20px", "margin-left": "15px", "font-family:" :"Open Sans"},
   };
+  const toolbarConfig = {
+    // Optionally specify the groups to display (displayed in the order listed).
+    display: [
+      "INLINE_STYLE_BUTTONS",
+      "BLOCK_TYPE_BUTTONS",
+      //   "LINK_BUTTONS",
+      "BLOCK_TYPE_DROPDOWN",
+      //   "HISTORY_BUTTONS",
+    ],
+    INLINE_STYLE_BUTTONS: [
+      { label: "Bold", style: "BOLD", className: "custom-css-class" },
+      { label: "Italic", style: "ITALIC" },
+      { label: "Underline", style: "UNDERLINE" },
+    ],
+    BLOCK_TYPE_DROPDOWN: [
+      { label: "Normal", style: "unstyled" },
+      { label: "Heading Large", style: "header-one" },
+      { label: "Heading Medium", style: "header-two" },
+      { label: "Heading Small", style: "header-three" },
+    ],
+    BLOCK_TYPE_BUTTONS: [
+      { label: "UL", style: "unordered-list-item" },
+      { label: "OL", style: "ordered-list-item" },
+    ],
+  };
 
 
 export default function PolicySettings() {
+
+    const fileTypes = ["doc", "pdf"];
+    const [govLawDesc, setgovLawDesc] = useState("");
+    const [govLawFile, setgovLawFile] = useState(null);
+
+    const [editorgovLawValue, setEditorgovLawValue] = React.useState(
+        RichTextEditor.createValueFromString(govLawDesc, "html")
+    );
+
   return (
     <div>
         <Row>
@@ -35,10 +72,40 @@ export default function PolicySettings() {
         </Row>
         <Row>
             <Col xs='12' sm='6' md='6' lg='6'>
-                
+                <RichTextEditor
+                toolbarConfig={toolbarConfig}
+                value={editorgovLawValue}
+                // onChange={handlegovLawChange}
+                required
+                id="body-text"
+                name="bodyText"
+                type="string"
+                multiline
+                variant="filled"
+                style={{
+                minHeight: 410,
+                width: 420,
+                marginTop: 20,
+                marginBottom: 20,
+                border: "1px solid black",
+                zIndex: 4,
+                textAlign: "left"
+                }}
+            />
             </Col>
             <Col xs='12' sm='6' md='6' lg='6'>
-
+                <FileUploader
+                // handleChange={handlegovLawFileChange}
+                name="file"
+                types={fileTypes}
+                children={
+                <UploadOrgLogo
+                    uploaddes="Supports: .doc, .pdf 2MB file size"
+                    uploadtitle="Upload Governing Laws "
+                />
+                }
+            // maxSize={2}
+          />
             </Col>
         </Row>
     </div>
