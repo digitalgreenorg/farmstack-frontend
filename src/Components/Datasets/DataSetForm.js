e05eafd65eb68bf3b119419136bac249f9f8ebc6
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-07-17 00:24:41 +0530

    added checkbox and text fields

--- src/Components/Datasets/DataSetForm.js
+++ src/Components/Datasets/DataSetForm.js
@@ -15,6 +15,9 @@ import RegexConstants from "../../Constants/RegexConstants";
 import TextareaAutosize from "@mui/base/TextareaAutosize";
 import HandleSessionTimeout, { handleUnwantedSpace } from "../../Utils/Common";
 import THEME_COLORS from "../../Constants/ColorConstants";
+import Checkbox from "@mui/material/Checkbox";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import { pink } from "@mui/material/colors";
 
 const useStyles = {
   btncolor: {
@@ -37,6 +40,8 @@ const useStyles = {
 export default function DataSetForm(props) {
   const [reply, setreply] = useState("");
   const [datasetname, setdatasetname] = useState("");
+  const [Geography, setGeography] = useState("");
+  const [cropdetail, setCropdetail] = useState("");
   return (
     <div className="datasetform">
       <Row>
@@ -80,6 +85,81 @@ export default function DataSetForm(props) {
               "min-height": "50px",
               "border-bottom": "1px solid #9AA1A9 !important",
             }}
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} sm={12} md={12} lg={12}>
+          <span className="AddDatasetsecondaryheading">
+            {/* {props.first_heading} */}
+            Data Category
+          </span>
+        </Col>
+      </Row>
+      <Row className="checkbox1">
+        <Col xs={12} sm={12} md={6} lg={3}>
+          <FormControlLabel control={<Checkbox />} label="Crop data" />
+        </Col>
+        <Col xs={12} sm={12} md={6} lg={3}>
+          <FormControlLabel control={<Checkbox />} label="Practice data" />
+        </Col>
+        <Col xs={12} sm={12} md={6} lg={3}>
+          <FormControlLabel control={<Checkbox />} label="Farmer profile" />
+        </Col>
+        <Col xs={12} sm={12} md={6} lg={3}>
+          <FormControlLabel control={<Checkbox />} label="Land records" />
+        </Col>
+      </Row>
+      <Row className="checkbox2">
+        <Col xs={12} sm={12} md={6} lg={3}>
+          <FormControlLabel control={<Checkbox />} label="Cultivation data" />
+        </Col>
+        <Col xs={12} sm={12} md={6} lg={3}>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Soil data"
+            className="soil"
+          />
+        </Col>
+        <Col xs={12} sm={12} md={6} lg={3}>
+          <FormControlLabel
+            control={<Checkbox />}
+            label="Weather data"
+            className="soil"
+          />
+        </Col>
+      </Row>
+      <Row>
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <TextField
+            // style={useStyles.inputwidth}
+            className="name"
+            id="filled-basic"
+            variant="filled"
+            required
+            value={Geography}
+            onChange={(e) =>
+              validateInputField(e.target.value, RegexConstants.ORG_NAME_REGEX)
+                ? setGeography(e.target.value)
+                : e.preventDefault()
+            }
+            label="Geography"
+          />
+        </Col>
+        <Col xs={12} sm={12} md={6} lg={6}>
+          <TextField
+            // style={useStyles.inputwidth}
+            className="crop"
+            id="filled-basic"
+            variant="filled"
+            required
+            value={cropdetail}
+            onChange={(e) =>
+              validateInputField(e.target.value, RegexConstants.ORG_NAME_REGEX)
+                ? setCropdetail(e.target.value)
+                : e.preventDefault()
+            }
+            label="Corp Detail"
           />
         </Col>
       </Row>
