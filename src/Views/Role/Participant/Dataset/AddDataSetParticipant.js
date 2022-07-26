007c537df5088c1283599cb1b0fb75f75cfb4444
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-07-26 16:03:19 +0530

    completed participant add dataset

--- src/Views/Role/Participant/Dataset/AddDataSetParticipant.js
+++ src/Views/Role/Participant/Dataset/AddDataSetParticipant.js
@@ -1,7 +1,24 @@
-import React from 'react'
+import React, { useState, useEffect } from "react";
+import Row from "react-bootstrap/Row";
+import Col from "react-bootstrap/Col";
+import { useHistory } from "react-router-dom";
+import AddDatasetParticipant from "../../../Dataset/DatasetParticipant/AddDatasetParticipant";
+import getTokenLocal from "../../../../Utils/Common";
 
 export default function AddDataSetParticipant() {
+  const history = useHistory();
+
   return (
-    <div>AddDataSetParticipant</div>
-  )
+    <>
+      <AddDatasetParticipant
+        isaccesstoken={() => getTokenLocal()}
+        okAction={() => history.push("/participant/datasets")}
+        cancelAction={() => history.push("/participant/home")}
+        isBackBtn={true}
+        successmsg={"Your dataset added successfully!"}
+        successimageText={"Success!"}
+        successheading={"Thank you for adding your dataset!"}
+      />
+    </>
+  );
 }
