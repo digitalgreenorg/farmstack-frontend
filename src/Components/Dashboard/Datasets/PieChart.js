c846e23a9560afa5aa46833ac3b78e48f4314cdc
Kanhaiya Suthar <kanhaiyasuthar@Kanhaiyas-MacBook-Pro.local>
2022-08-10 12:44:52 +0530

    some css fixed

--- src/Components/Dashboard/Datasets/PieChart.js
+++ src/Components/Dashboard/Datasets/PieChart.js
@@ -1,51 +1,82 @@
-import React, { PureComponent, useEffect } from 'react';
-import { PieChart, Pie, Sector, Cell, ResponsiveContainer, Tooltip, Legend } from 'recharts';
-import PieChartsSideMenu from './PieChartsSideMenu';
-import styles from "./datasets.module.css"
-import LegendValue from './LegendValue';
+import React, { PureComponent, useEffect } from "react";
+import {
+  PieChart,
+  Pie,
+  Sector,
+  Cell,
+  ResponsiveContainer,
+  Tooltip,
+  Legend,
+} from "recharts";
+import PieChartsSideMenu from "./PieChartsSideMenu";
+import styles from "./datasets.module.css";
+import LegendValue from "./LegendValue";
 
 const data = [
-  { name: 'Crop data', value: 400 },
-  { name: 'Land record', value: 300 },
-  { name: 'Farmers data', value: 300 },
-  { name: 'Pricing data', value: 200 },
-  { name: 'Insurance', value: 400 },
-  { name: 'Credit Assessment', value: 100 },
+  { name: "Crop data", value: 400 },
+  { name: "Land record", value: 300 },
+  { name: "Farmers data", value: 300 },
+  { name: "Pricing data", value: 200 },
+  { name: "Insurance", value: 400 },
+  { name: "Credit Assessment", value: 100 },
 ];
-let totalSum= 0;
-data.forEach((item)=>totalSum+= +item.value);
+let totalSum = 0;
+data.forEach((item) => (totalSum += +item.value));
 
-
-const COLORS = ['#E7B100', '#402769', '#FD7B25', '#836400','#096D0D', '#3BBFCC'];
+const COLORS = [
+  "#E7B100",
+  "#402769",
+  "#FD7B25",
+  "#836400",
+  "#096D0D",
+  "#3BBFCC",
+];
 
 const RADIAN = Math.PI / 180;
-const renderCustomizedLabel = ({ cx, cy, midAngle, innerRadius, outerRadius, percent, index }) => {
+const renderCustomizedLabel = ({
+  cx,
+  cy,
+  midAngle,
+  innerRadius,
+  outerRadius,
+  percent,
+  index,
+}) => {
   const radius = innerRadius + (outerRadius - innerRadius) * 0.5;
   const x = cx + radius * Math.cos(-midAngle * RADIAN);
   const y = cy + radius * Math.sin(-midAngle * RADIAN);
 
   return (
-    <text x={x} y={y} fill="white" textAnchor={x > cx ? 'start' : 'end'} dominantBaseline="central">
+    <text
+      x={x}
+      y={y}
+      fill="white"
+      textAnchor={x > cx ? "start" : "end"}
+      dominantBaseline="central"
+    >
       {`${(percent * 100).toFixed(0)}%`}
     </text>
   );
 };
 
-
-
 export default class PieChartMain extends PureComponent {
-//   static demoUrl = 'https://codesandbox.io/s/pie-chart-with-customized-label-dlhhj';
-
-
+  //   static demoUrl = 'https://codesandbox.io/s/pie-chart-with-customized-label-dlhhj';
 
   render() {
     return (
-  
-    <div className={styles.datasetsChartBox}>
-        <PieChart   width={640} height={317}>
+      <div className={styles.datasetsChartBox}>
+        <PieChart
+          style={{
+            // border: "1px solid green",
+            display: "flex",
+            justifyContent: "left",
+          }}
+          width={640}
+          height={317}
+        >
           <Pie
             data={data}
-            cx="35%"
+            cx="40%"
             cy="50%"
             labelLine={false}
             // label={renderCustomizedLabel}
@@ -54,36 +85,39 @@ export default class PieChartMain extends PureComponent {
             dataKey="value"
             animationEasing="ease-out"
             animationBegin={500}
-              animationDuration={3000}
-            
-              
-            >
+            animationDuration={3000}
+          >
             {data.map((entry, index) => (
-                <Cell key={`cell-${index}`} fill={COLORS[index % COLORS.length]} />
-                ))}
+              <Cell
+                key={`cell-${index}`}
+                fill={COLORS[index % COLORS.length]}
+              />
+            ))}
           </Pie>
-          <Tooltip  itemStyle={{color:"#3D4A52"}} />
-          <Legend 
-        
-  //          payload={
-  //   data.map(
-  //     (item, index) => ({
-  //       id: item.name,
-  //       type: "square",
-  //       value: `${Math.round((item.value/totalSum) * 100)}% ${item.name}`,
-  //       color: COLORS[index % COLORS.length]
-  //     })
-  //   )
-  // }
-  content={<LegendValue data={data} COLORS={COLORS} />}
- verticalAlign="middle" align='right' layout='vertical' />
+          <Tooltip
+            itemStyle={{ color: "#3D4A52" }}
+            wrapperStyle={{ color: "green" }}
+          />
+          <Legend
+            //          payload={
+            //   data.map(
+            //     (item, index) => ({
+            //       id: item.name,
+            //       type: "square",
+            //       value: `${Math.round((item.value/totalSum) * 100)}% ${item.name}`,
+            //       color: COLORS[index % COLORS.length]
+            //     })
+            //   )
+            // }
+            content={<LegendValue data={data} COLORS={COLORS} />}
+            verticalAlign="middle"
+            align="right"
+            layout="vertical"
+          />
         </PieChart>
 
-    
-           {/* <PieChartsSideMenu data={data} COLORS={COLORS} /> */}
-
-                </div>
-
+        {/* <PieChartsSideMenu data={data} COLORS={COLORS} /> */}
+      </div>
     );
   }
 }
--- src/Components/Dashboard/TodoList/TodoListBigBox.js
+++ src/Components/Dashboard/TodoList/TodoListBigBox.js
@@ -1,22 +1,36 @@
-import React from 'react'
-import styles from './todoListBigBox.module.css';
+import React from "react";
+import styles from "./todoListBigBox.module.css";
 import Dummy from "../../../Assets/Img/dummy.png";
-import labels from '../../../Constants/labels'
+import labels from "../../../Constants/labels";
 
-const TodoListBigBox = ({totalDetail}) => {
+const TodoListBigBox = ({ totalDetail }) => {
   return (
     <div className={styles.todoListEachBigBox}>
-
-    <div className={styles.todoListBigBoxUpperPortion}>
+      <div className={styles.todoListBigBoxUpperPortion}>
         <span className={styles.totalDetailsTitle}>{totalDetail.title}</span>
-        <img className={styles.todoListBigBoxUpperPortionImg} src={totalDetail.imgUrl}/>
+        <img
+          className={styles.todoListBigBoxUpperPortionImg}
+          src={totalDetail.imgUrl}
+        />
         {/* <img className={styles.todoListBigBoxUpperPortionImg} src={Dummy}/> */}
         {/* <img className={styles.todoListBigBoxUpperPortionImg} src= "."/> */}
+      </div>
+      {/* <div className="nodataavailable">SomeClass</div> */}
+      <span className={styles.todoListBigBoxLowerValues}>
+        {" "}
+        <span style={{ height: "54px" }}> {totalDetail.value}</span>{" "}
+        <span
+          style={{
+            fontSize: "18px",
+            // border: "1px solid blue",
+            height: "25px",
+          }}
+        >
+          {totalDetail.valueUnit ? totalDetail.valueUnit : null}
+        </span>{" "}
+      </span>
     </div>
-    {/* <div className="nodataavailable">SomeClass</div> */}
-    <span className={styles.todoListBigBoxLowerValues}> <span> {totalDetail.value}</span> <sub style={{fontSize:"18px"}}>{totalDetail.valueUnit? totalDetail.valueUnit: null}</sub>  </span>
-    </div>
-  )
-}
+  );
+};
 
-export default TodoListBigBox
+export default TodoListBigBox;
