18d3618ef517b04f15a2740099b67ce11aea5972
Jayasankar G K <jaykay@Jaykays-MacBook-Pro.local>
2022-08-12 16:17:52 +0530

    connector card field ellipsis, dataset filter reset on tab change

--- src/Views/Dataset/DatasetAdmin/DatasetAdmin.js
+++ src/Views/Dataset/DatasetAdmin/DatasetAdmin.js
@@ -475,6 +475,8 @@ export default function DatasetAdmin() {
                 // setCropFilterMaster(initFilter(cropFilterInput))
                 setCropFilterDisplay(initFilter(cropFilterInput))
 
+                resetFilterState(screenlabels.dataset.age)
+
                 // console.log("geoFilterMaster", geoFilterMaster)
                 console.log("geoFilterDisplay", geoFilterDisplay)
                 // console.log("cropFilterMaster",cropFilterMaster)
