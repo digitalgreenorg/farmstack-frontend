7ad67b07fcad0d1c264ae662641e8ccf30593f60
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-08-15 15:19:03 +0530

    added regex for connector name

--- src/Constants/RegexConstants.js
+++ src/Constants/RegexConstants.js
@@ -1,18 +1,21 @@
 const RegexConstants = {
-    APLHABET_REGEX : /[A-Za-z]/,
-    TEXT_REGEX : /^[A-Za-z]*$/,
-    NUMBER_REGEX : /^[0-9]*$/,
-    // DIGIT_REGEX : /[0-9]/,
-    ORG_NAME_REGEX : /^$|^[A-za-z0-9](([^\s])+(\s)?)*$/,
-    INVALID_ADDRESS_CHARACTERS : /[!@$%^*(){}\[\]:;"'?]/,
-    PINCODE_REGEX : /^[0-9]{0,10}$/,
-    WEBSITE_URL_REGEX : /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/,
-    NO_SPACE_REGEX : /^([^\s])*$/,
-//   DATE_SET_REGEX: /^$|^[A-za-z][0-9]?(([^\s])+(\s)?)*$/,
+  APLHABET_REGEX: /[A-Za-z]/,
+  TEXT_REGEX: /^[A-Za-z]*$/,
+  NUMBER_REGEX: /^[0-9]*$/,
+  // DIGIT_REGEX : /[0-9]/,
+  ORG_NAME_REGEX: /^$|^[A-za-z0-9](([^\s])+(\s)?)*$/,
+  INVALID_ADDRESS_CHARACTERS: /[!@$%^*(){}\[\]:;"'?]/,
+  PINCODE_REGEX: /^[0-9]{0,10}$/,
+  WEBSITE_URL_REGEX:
+    /^((https?|ftp|smtp):\/\/)?(www.)?[a-z0-9]+\.[a-z]+(\/[a-zA-Z0-9#]+\/?)*$/,
+  NO_SPACE_REGEX: /^([^\s])*$/,
+  //   DATE_SET_REGEX: /^$|^[A-za-z][0-9]?(([^\s])+(\s)?)*$/,
   DATA_SET_REGEX: /^$|^[a-zA-Z][A-Za-z0-9_&@-]?(([^\s])+(\s)?)*$/,
+  // connector_name: /^$|^[a-zA-Z0-9][A-Za-z0-9]?(([^\s])+(\s)?)*$/,
+  connector_name: /^$|^[a-zA-Z0-9][a-zA-Z0-9 ]*$/,
   CROP_SET_REGEX: /^$|^[a-zA-Z][ A-Za-z0-9_-]*$/,
   GEO_SET_REGEX: /^$|^[a-zA-Z\s]?(([^\s])+(\s)?)*$/,
-  DES_SET_REGEX: /^$|^[a-zA-Z][A-Za-z0-9]?(([^\s])+(\s)?)*$/
- };
+  DES_SET_REGEX: /^$|^[a-zA-Z][A-Za-z0-9]?(([^\s])+(\s)?)*$/,
+};
 
-export default RegexConstants
+export default RegexConstants;
