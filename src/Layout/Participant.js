6d601bb8af5f9d55f22b9c956a6e05b316aa67cd
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-08-24 14:17:49 +0530

    added base file and routing for add and edit projects

--- src/Layout/Participant.js
+++ src/Layout/Participant.js
@@ -19,8 +19,11 @@ import EditConnectorParticipant from "../Views/Role/Participant/Connectors/EditC
 import ConnectorParticipant from "../Views/Connector/ConnectorParticipant/ConnectorParticipant";
 import DepartmentSettings from "../Views/Settings/ParticipantSettings/DepartmentSettings";
 import EditDepartmentSettings from "../Views/Settings/ParticipantSettings/EditDepartmentSettings";
-import ViewDepartment from "../Views/Settings/ParticipantSettings/ViewDepartment"
+import ViewDepartment from "../Views/Settings/ParticipantSettings/ViewDepartment";
 import Footer from "../Components/Footer/Footer";
+
+import AddProjectParticipant from "../Views/Settings/ParticipantSettings/Project/AddProjectParticipant";
+import EditProjectParticipant from "../Views/Settings/ParticipantSettings/Project/EditProjectParticipant";
 
 function Participant(props) {
   return (
@@ -29,65 +32,75 @@ function Participant(props) {
         <div className="center_keeping_conatiner">
           <ParticipantNavbar />
           <div className="minHeight67vhParticipantPage">
-          <Switch>
-            <Route
-              exact
-              path="/participant/datasets"
-              component={DatasetParticipant}
-            />
-            <Route
-              exact
-              path="/participant/connectors"
-              component={ConnectorParticipant}
-            />
-            <Route exact path="/participant/home" component={Home} />
-            <Route
-              exact
-              path="/participant/datasets/add"
-              component={AddDataSetParticipant}
-            />
-            <Route
-              exact
-              path="/participant/datasets/edit/:id"
-              component={EditDatasetParticipant}
-            />
-            <Route
-              exact
-              path="/participant/connectors/add"
-              component={AddConnectorParticipant}
-            />
-            <Route
-              exact
-              path="/participant/connectors/edit/:id"
-              component={EditConnectorParticipant}
-            />
-            <Route
-              exact
-              path="/participant/settings/:id"
-              component={Participantsettings}
-            />
-            <Route
-              exact
-              path="/participant/settings/adddepartment"
-              component={DepartmentSettings}
-            />
-            <Route
-              exact
-              path="/participant/settings/editdepartment/:id"
-              component={EditDepartmentSettings}
-            />
-            <Route
-              exact
-              path="/participant/settings/viewdepartment/:id/"
-              component={ViewDepartment}
-            />
-          </Switch>
+            <Switch>
+              <Route
+                exact
+                path="/participant/datasets"
+                component={DatasetParticipant}
+              />
+              <Route
+                exact
+                path="/participant/connectors"
+                component={ConnectorParticipant}
+              />
+              <Route exact path="/participant/home" component={Home} />
+              <Route
+                exact
+                path="/participant/datasets/add"
+                component={AddDataSetParticipant}
+              />
+              <Route
+                exact
+                path="/participant/datasets/edit/:id"
+                component={EditDatasetParticipant}
+              />
+              <Route
+                exact
+                path="/participant/connectors/add"
+                component={AddConnectorParticipant}
+              />
+              <Route
+                exact
+                path="/participant/connectors/edit/:id"
+                component={EditConnectorParticipant}
+              />
+              <Route
+                exact
+                path="/participant/settings/:id"
+                component={Participantsettings}
+              />
+              <Route
+                exact
+                path="/participant/settings/adddepartment"
+                component={DepartmentSettings}
+              />
+              <Route
+                exact
+                path="/participant/settings/editdepartment/:id"
+                component={EditDepartmentSettings}
+              />
+              <Route
+                exact
+                path="/participant/settings/viewdepartment/:id/"
+                component={ViewDepartment}
+              />
+              <Route
+                exact
+                path="/participant/settings/project/add"
+                component={AddProjectParticipant}
+              />
+              <Route
+                exact
+                path="/participant/settings/project/edit/:id"
+                component={EditProjectParticipant}
+              />
+            </Switch>
           </div>
-          <Footer/>
+          <Footer />
         </div>
       ) : (
         props.history.push("/login")
-        )}
+      )}
     </>
   );
 }
