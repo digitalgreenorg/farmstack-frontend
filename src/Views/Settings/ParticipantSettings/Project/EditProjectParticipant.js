7bad669ad2a3a09ea410eca81323d9540afafd14
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-08-24 17:10:17 +0530

    added form components to add and edit project in participants

--- src/Views/Settings/ParticipantSettings/Project/EditProjectParticipant.js
+++ src/Views/Settings/ParticipantSettings/Project/EditProjectParticipant.js
@@ -1,10 +1,55 @@
 import React from "react";
 import ProjectForm from "../../../../Components/Settings/Participants/Project/ProjectForm";
 
+import {
+  validateInputField,
+  handleUnwantedSpace,
+} from "../../../../Utils/Common";
+import RegexConstants from "../../../../Constants/RegexConstants";
+
 export default function EditProjectParticipant() {
+  const [department, setdepartment] = React.useState("");
+  const [project, setproject] = React.useState("");
+  const [description, setdescription] = React.useState("");
+
+  const [department_variable, setdepartment_variable] = React.useState([]);
+
+  const handleChangeDepartment = (event) => {
+    console.log(event.target.value);
+    setdepartment(event.target.value);
+  };
+  const handleChangeProject = (e) => {
+    validateInputField(e.target.value, RegexConstants.connector_name)
+      ? setproject(e.target.value)
+      : e.preventDefault();
+
+    console.log(e.target.value);
+
+    // setproject(e.target.value);
+  };
+
+  const handleChangedescription = (e) => {
+    console.log(e.target.value);
+    validateInputField(e.target.value, RegexConstants.DES_SET_REGEX)
+      ? setdescription(e.target.value)
+      : e.preventDefault();
+  };
+  const handledescriptionKeydown = (e) => {
+    handleUnwantedSpace(description, e);
+  };
   return (
     <>
-      <ProjectForm />
+      <ProjectForm
+        title={"Edit a project"}
+        department={department}
+        project={project}
+        description={description}
+        department_variable={department_variable}
+        handleChangeDepartment={handleChangeDepartment}
+        handleChangeProject={handleChangeProject}
+        handleChangedescription={handleChangedescription}
+        handledescriptionKeydown={handledescriptionKeydown}
+      />
     </>
   );
 }
