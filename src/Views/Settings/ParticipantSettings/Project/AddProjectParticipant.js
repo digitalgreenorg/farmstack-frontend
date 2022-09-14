5e0381334647b74abbfc9be17e1ede9a24803207
Jashwanth-Gowda-R <jashwanth.go@gmail.com>
2022-09-14 16:38:53 +0530

    added default in egt request in project

--- src/Views/Settings/ParticipantSettings/Project/AddProjectParticipant.js
+++ src/Views/Settings/ParticipantSettings/Project/AddProjectParticipant.js
@@ -159,7 +159,7 @@ export default function AddProjectParticipant() {
     await HTTPService(
       "GET",
       UrlConstants.base_url + UrlConstants.departments_connector_list,
-      { org_id: getOrgLocal() },
+      { org_id: getOrgLocal(), default: false },
       false,
       true
     )
