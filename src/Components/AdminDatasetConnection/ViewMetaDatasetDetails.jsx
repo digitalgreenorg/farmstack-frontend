9dd4978a97c25e718862d13c290052c7b8607bdb
Shruthi <shruthichandran@digitalgreen.org>
2023-01-16 18:09:12 +0530

    review changes done

--- src/Components/AdminDatasetConnection/ViewMetaDatasetDetails.jsx
+++ src/Components/AdminDatasetConnection/ViewMetaDatasetDetails.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import { useState, useEffect } from "react"
-import { Row, Col,} from "react-bootstrap"
+import { Row, Col, Button } from "react-bootstrap"
 import "./LocalMachineUploadDataset.css"
 import labels from "../../Constants/labels";
 // import Loader from '../Loader/Loader';
@@ -17,6 +17,8 @@ import TableHead from "@mui/material/TableHead";
 import TableRow from "@mui/material/TableRow";
 import { Avatar } from '@mui/material';
 import FileDownloadSharpIcon from '@mui/icons-material/FileDownloadSharp';
+import parse from 'html-react-parser'
+
 
 
 
@@ -25,19 +27,18 @@ export default function ViewMetaDatasetDetails(props) {
     const [screenlabels, setscreenlabels] = useState(labels["en"]);
     const [datasetdescription, setDatasetDescription] = useState("")
     const [category, setCategory] = useState([])
-    // const [subcategory, setSubCategory] = useState([]);
     const [geography, setGeography] = useState(null);
     const [constantlyupdate, setconstantlyupdate] = useState(null);
     const [fromdate, setFromdate] = useState(null)
     const [toDate, setToDate] = useState(null)
     const [isFileDataLoaded, setFileDataLoaded] = useState(false);
-    // const [tableValues, settableValues] = useState([]);
     const [isLoading, setLoader] = useState(false)
     const { id } = useParams()
     const history = useHistory();
     const [fileData, setfileData] = useState([])
     const [orgdetail, setOrgDetail] = useState("")
     const [userdetails, setUserDetails] = useState("")
+    const [orgdes, setorgdes] = useState("")
     const fileTypes = ["XLS", "xlsx", "CSV", "PDF", "JPEG", "JPG", "PNG", "TIFF"]
 
     useEffect(() => {
@@ -48,10 +49,10 @@ export default function ViewMetaDatasetDetails(props) {
         setFileDataLoaded(true)
     }, [fileData])
 
-    let url = UrlConstant.base_url + UrlConstant.datasetview + id + '/' ;
+    let url = UrlConstant.base_url + UrlConstant.datasetview + id + '/';
     const getMetaData = () => {
         console.log("get details of the form")
-          setLoader(true)
+        setLoader(true)
         HTTPService(
             "GET",
             url,
@@ -59,7 +60,7 @@ export default function ViewMetaDatasetDetails(props) {
             false,
             false
         ).then((response) => {
-             setLoader(false)
+            setLoader(false)
             console.log(response.data)
             setCategory(response.data.category)
             setGeography(response.data.geography)
@@ -69,10 +70,10 @@ export default function ViewMetaDatasetDetails(props) {
             setDatasetDescription(response.data.description)
             setfileData(response.data.datasets)
             setOrgDetail(response.data.organization)
+            setorgdes(response.data.organization.org_description)
             setUserDetails(response.data.user)
-
         }).catch((e) => {
-             setLoader(false);
+            setLoader(false);
             history.push(GetErrorHandlingRoute(e));
         }
 
@@ -81,6 +82,16 @@ export default function ViewMetaDatasetDetails(props) {
     }
     return (<>
 
+        <Row>
+            <Col className="supportViewDetailsbackimage">
+                <span onClick={() => props.back()}>
+                    <img src={require("../../Assets/Img/Vector.svg")} alt="new" />
+                </span>
+                <span className="supportViewDetailsback" onClick={() => history.push("/datahub/datasets")}>
+                    {"Back"}
+                </span>
+            </Col>
+        </Row>
         <Row className='main_heading_row'>
             <Col lg={3} sm={6} style={{ marginLeft: "99px", marginTop: "50px", }}>
                 <span className='Main_heading_add_dataset'>Dataset Details</span>
@@ -96,28 +107,28 @@ export default function ViewMetaDatasetDetails(props) {
             <Col style={{ "margin-top": "40px" }}>
                 <Row className="secondmainheading">Category</Row>
                 {category.map((data) => (
-                    <Row key={category} value={category} className="thirdmainheading" style={{ "text-align" :  "center", "alignItems" : "center",  "margin-top": "10px", "border-radius": "10px", "border": "2px solid #83A9C9", "background": "#83A9C9", "width": "150px", "height": "40px"  }}>
+                    <Row key={category} value={category} className="thirdmainheading" style={{ "text-align": "center", "alignItems": "center", "margin-top": "10px", "border-radius": "10px", "border": "2px solid #83A9C9", "background": "#83A9C9", "width": "150px", "height": "40px" }}>
                         {data.category}</Row>))}
             </Col>
             <Col style={{ "margin-top": "40px" }}>
                 <Row className="secondmainheading">Sub Category</Row>
                 {category.map((item1) => item1.children.map((item2) => (
-                    <Row className="thirdmainheading" style={{ "margin-top": "10px", "border-radius": "10px", "border": "2px solid #8AA7AD", "background": "#8AA7AD", "width": "150px", "height": "40px", "text-align" :  "center", "alignItems" : "center", }}>
+                    <Row className="thirdmainheading" style={{ "margin-top": "10px", "border-radius": "10px", "border": "2px solid #8AA7AD", "background": "#8AA7AD", "width": "150px", "height": "40px", "text-align": "center", "alignItems": "center", }}>
                         {item2.sub_category}</Row>)))}
             </Col>
             <Col style={{ "margin-top": "40px" }}>
                 <Row className="secondmainheading">Geography</Row>
-                <Row className="thirdmainheading" style={{ "margin-top": "10px", "border-radius": "10px", "border": "2px solid #9ABA8F", "background": "#9ABA8F", "width": "150px",  "height": "40px", "text-align" :  "center", "alignItems" : "center",  }}>
+                <Row className="thirdmainheading" style={{ "margin-top": "10px", "border-radius": "10px", "border": "2px solid #9ABA8F", "background": "#9ABA8F", "width": "150px", "height": "40px", "text-align": "center", "alignItems": "center", }}>
                     {geography}</Row>
             </Col>
             <Col style={{ "margin-top": "40px" }}>
                 <Row className="secondmainheading">Freshness of Data</Row>
-                <Row className="thirdmainheading" style={{ "margin-top": "10px", "border-radius": "10px", "border": "2px solid #DFC780", "background": "#DFC780", "width": "150px", "height": "40px", "text-align" :  "center", "alignItems" : "center", }}>
+                <Row className="thirdmainheading" style={{ "margin-top": "10px", "border-radius": "10px", "border": "2px solid #DFC780", "background": "#DFC780", "width": "150px", "height": "40px", "text-align": "center", "alignItems": "center", }}>
                     {constantlyupdate ? "Yes" : "No"}</Row>
             </Col>
             <Col style={{ "margin-top": "40px" }}>
                 <Row className="secondmainheading">Data Capture Interval</Row>
-                <Row className="thirdmainheading" style={{ "margin-top": "10px", "border-radius": "10px", "border": "2px solid #D9B082", "background": "#D9B082", "width": "150px", "height": "40px", "text-align" :  "center", "alignItems" : "center", }}>
+                <Row className="thirdmainheading" style={{ "margin-top": "10px", "border-radius": "10px", "border": "2px solid #D9B082", "background": "#D9B082", "width": "150px", "height": "40px", "text-align": "center", "alignItems": "center", }}>
                     <span>{fromdate ? fromdate : "NA"}</span>
                     <span>{toDate}</span>
                 </Row>
@@ -154,6 +165,7 @@ export default function ViewMetaDatasetDetails(props) {
                 </Alert>
             </Stack>
         </Row>
+
         <Row
             style={{
                 border: "1px solid #DFDFDF",
@@ -172,30 +184,49 @@ export default function ViewMetaDatasetDetails(props) {
                 >
                     <TableHead>
 
-                        <TableRow>
 
-                            {isFileDataLoaded && fileData.map(itm1 => {
-                                return itm1.content.map(itm2 => {
-                                    return Object.keys(itm2).map(itm3 => {
-                                        return <TableCell> {itm3} </TableCell>
-                                    })
-                                })
 
-                            })}
+                        {isFileDataLoaded && fileData.map(itm1 => {
+                            return (
+                                <>
+                                    {itm1.content.map((itm2, i) => {
+                                        if (i === 0) {
+                                            return (
+                                                <TableRow>
+                                                    {Object.keys(itm2).map((itm3, i) => {
+                                                        return i !== 0 ? <TableCell> {itm3} </TableCell> : ""
+                                                    })
 
-                        </TableRow>
+                                                    }
+                                                </TableRow>
+
+                                            )
+                                        }
+                                    })}
+                                </>
+
+                            )
+                        })}
+
+
                     </TableHead>
                     <TableBody>
-                        <TableRow >
-                            {isFileDataLoaded && fileData.map(itm1 => {
-                                return itm1.content.map(itm2 => {
-                                    return Object.values(itm2).map(itm3 => {
-                                        return <TableCell> {itm3} </TableCell>
-                                    })
-                                })
 
-                            })}
-                        </TableRow>
+                        {isFileDataLoaded && fileData.map(itm1 => {
+                            return itm1.content.map(itm2 => {
+                                return (
+                                    <TableRow>
+                                        {
+                                            Object.values(itm2).map((itm3, i) => {
+                                                return i !== 0 ? <TableCell> {itm3} </TableCell> : ""
+                                            })
+                                        }
+                                    </TableRow>
+                                )
+                            })
+
+                        })}
+
                     </TableBody>
                 </Table>
             </Col>
@@ -208,14 +239,14 @@ export default function ViewMetaDatasetDetails(props) {
                         className="fontweight600andfontsize14pxandcolor3D4A52 supportcardsecondcolumn"
                         style={{ "margin-top": "10px", cursor: "pointer", "marginLeft": "130px" }}
                         onClick={() =>
-                            downloadAttachment(UrlConstant.base_url + downloadfile.file,downloadfile.file.split("/")[5] )
+                            downloadAttachment(UrlConstant.base_url + downloadfile.file, downloadfile.file.split("/")[5])
                         }
                     >
-                       <FileDownloadSharpIcon
-                       style={{marginRight: "20px"}} />
+                        <FileDownloadSharpIcon
+                            style={{ marginRight: "20px" }} />
                         <Row>{downloadfile.file.split("/")[5]}
-                        <hr className="separatorline" />
-                        {/* m */}
+                            <hr className="separatorline" />
+                            {/* m */}
                         </Row>
                     </Row>))}
                 <hr className="separatorline" />
@@ -225,36 +256,36 @@ export default function ViewMetaDatasetDetails(props) {
             <Col style={{ "marginLeft": "30px", "margin-top": "50px" }}>
                 <Row className="secondmainheading">
                     <Col style={{ "margin-left": "-130px" }}>ORGANISATION</Col>
-                
-                
+
+
                 </Row>
                 <Col className="thirdmainheading" style={{ "marginLeft": "-17px", "margin-top": "30px" }}>
-                <Col>
-                    <Avatar alt="Remy Sharp" 
-                    // className='css-v8h2xp-MuiAvatar-root'
-                     style={{"margin-left": "-40"}}
-                     src={UrlConstant.base_url+orgdetail.logo} 
-                    sx={{ width:44, height:44 }}/>
+                    <Col>
+                        <Avatar alt="Remy Sharp"
+                            // className='css-v8h2xp-MuiAvatar-root'
+                            style={{ "margin-left": "-40" }}
+                            src={UrlConstant.base_url + orgdetail.logo}
+                            sx={{ width: 44, height: 44 }} />
                     </Col>
-                    <Col style={{"margin-left" : "90px", "marginTop" : "-30px"}}>
-                    <Row>{orgdetail.name}</Row>
-                    <Row>{orgdetail.org_email}</Row>
-                    <Row>{orgdetail.org_description}</Row>
-                    <Row>{orgdetail.phone_number}</Row>
-                    <Row>{orgdetail?.address?.city}</Row>
-                    <Row>{orgdetail?.address?.country}</Row>
-                    <Row>{orgdetail?.address?.address}</Row>
-                    <Row>{orgdetail?.address?.pincode}</Row>
+                    <Col style={{ "margin-left": "90px", "marginTop": "-30px" }}>
+                        <Row>{orgdetail.name}</Row>
+                        <Row>{orgdetail.org_email}</Row>
+                        <Row style={{ "margin-bottom": "-15px" }}>{parse(orgdes)}</Row>
+                        <Row>{orgdetail.phone_number}</Row>
+                        <Row>{orgdetail?.address?.city}</Row>
+                        <Row>{orgdetail?.address?.country}</Row>
+                        <Row>{orgdetail?.address?.address}</Row>
+                        <Row>{orgdetail?.address?.pincode}</Row>
                     </Col>
                 </Col>
             </Col>
             <Col><div style={{
                 marginLeft: "-100px",
                 marginTop: "40px"
-            }}className='horizontal'></div></Col>
+            }} className='horizontal'></div></Col>
 
             <Col style={{ "marginLeft": "60px", "margin-top": "30px" }}>
-                <Row style={{"marginLeft" : "-550px", "margin-top": "25px"}}>ROOT USER DETAILS</Row>
+                <Row style={{ "marginLeft": "-550px", "margin-top": "25px" }}>ROOT USER DETAILS</Row>
                 <Col className="thirdmainheading" style={{ "marginLeft": "-550px", "margin-top": "38px" }}>
                     <Row>{userdetails.first_name}</Row>
                     <Row>{userdetails.last_name}</Row>
@@ -262,7 +293,33 @@ export default function ViewMetaDatasetDetails(props) {
                 </Col>
             </Col>
         </Row>
-
+        <Row className="marginrowtop8px"></Row>
+        <Row>
+            <Col xs={12} sm={12} md={6} lg={3}></Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+                <Button
+                    onClick={() => props.edit()}
+                    variant="outlined"
+                    className="submitbtn"
+                >
+                    Edit
+                </Button>
+            </Col>
+        </Row>
+        <Row className="margin">
+            <Col xs={12} sm={12} md={6} lg={3}></Col>
+            <Col xs={12} sm={12} md={6} lg={6}>
+                <Button
+                    onClick={() => history.push("/datahub/datasets")}
+                    style={{ "margin-top": "0px" }}
+                    variant="outlined"
+                    className="editbtn"
+                >
+                    Cancel
+                </Button>
+            </Col>
+        </Row>
+        <Row className="marginrowtop8px"></Row>
 
     </>
     )
