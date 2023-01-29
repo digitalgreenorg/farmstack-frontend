be844df2837c26f3546ae7ec419b713ba27b343b
Shruthi <shruthichandran@digitalgreen.org>
2023-01-29 20:49:07 +0530

    accordian fixed

--- src/Components/AdminDatasetConnection/FileUploaderDetailsAccordian.jsx
+++ src/Components/AdminDatasetConnection/FileUploaderDetailsAccordian.jsx
@@ -7,48 +7,52 @@ import { Row, Col } from "react-bootstrap";
 
 export default function FileUploaderDetailsAccordian({ data, title, deleteFunc, source, datasetname, loader, key, setKey, uploadFile, localUploaded }) {
 
-    return ( <>
-            <Col>
-            {/* {(uploadFile.length != 0) && localUploaded? */}
+    return ( 
            <Accordion defaultExpanded={true}>
                <AccordionSummary
                 expandIcon={<ExpandMoreIcon />}
                 aria-controls="panel1a-content"
                 id="panel1a-header"
-            ></AccordionSummary>
+            >
+             <Typography sx={{ width: '33%', flexShrink: 0 }}>
+                    {title}
+
+              <sup style={{ height: "20px", width: "20px", backgroundColor: "green", color: "white", borderRadius: "10px", padding: "0px 5px", fontSize: "small", margin: "0px 5px" }}>{uploadFile?.length}  </sup>
+                </Typography>
+              </AccordionSummary>
              { uploadFile ?
              (<AccordionDetails>
-              <Row style={{ maxHeight: "200px", overflowY: "scroll", }}>
-                {/* {uploadFile ? */}
+              <Typography style={{ maxHeight: "300px", overflowY: "scroll" , overflowX: "hidden"}}>
                   <ol className="uploaddatasetname">
                     {uploadFile.map((item) => {
                       return (<> 
                       <Row key={key}>
-                      <Col>
+                      {/* <Col> */}
                         <li className="uploadList">
                           {item.name}
                         </li>
-                        </Col>
-                        <Col style={{marginLeft: "50px"}}>
-                        <IconButton edge="end" aria-label="delete">
+                        {/* </Col> */}
+                        {/* <Col > */}
+                        <IconButton edge="end" aria-label="delete"
+                        // style={{alignItem : "right", "marginRight": "80px"}}
+                        >
                           <DeleteOutlinedIcon
+                          
                             onClick={() => (deleteFunc(datasetname, source, item.name))}
                             color='warning' />
                         </IconButton>
-                        </Col>
+                        {/* </Col> */}
+                        {/* </Row> */}
+                        <LinearProgress variant="determine" value={item?.progress ? item?.progress : 0} key={key} color="success"
+                        style={{width : "350px"}} />
                         </Row>
-                        <LinearProgress variant="determine" value={item?.progress ? item?.progress : 0} key={key} color="success" />
                         <p>{item?.progress ? item?.progress : 0}%</p>
                       </>
                       )
                     })}
                   </ol>
-              </Row>
+              </Typography>
               </AccordionDetails>) : ("")}
               </Accordion> 
-
-                 
-            </Col>
-</>
     )
                   }
