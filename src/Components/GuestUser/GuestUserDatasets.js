4973c5a5adf24f7c7f3540686145034387205abe
Nilesh Tiwari <nilesh@Nileshs-MacBook-Pro.local>
2023-02-20 14:33:44 +0530

    fixed date fillter issue on home page

--- src/Components/GuestUser/GuestUserDatasets.js
+++ src/Components/GuestUser/GuestUserDatasets.js
@@ -875,14 +875,10 @@ export default function GuestUserDatasets() {
   const filterByDates = () => {
     let fromDateandToDate = [];
     fromDateandToDate.push(new Date(fromdate.getTime() - (fromdate.getTimezoneOffset() * 60000)).toJSON());
-    fromDateandToDate.push(new Date(todate.getTime() - (todate.getTimezoneOffset() * 60000)).toJSON());
+    // Adding 86400000 will add 1 more day in date (86400000 == 24hrs)
+    fromDateandToDate.push(new Date(todate.getTime() - (todate.getTimezoneOffset() * 60000) + 86400000).toJSON());
     console.log('payload in date fillter api',fromDateandToDate, fromdate, todate)
 
-    // let date = new Date(fromdate)
-
-    // console.log('date', date, JSON.stringify(date.toString().split('00:00:00')[0]), typeof fromdate, JSON.stringify(fromdate))
-
-    // console.log('date in IST',new Date(fromdate.getTime() - (fromdate.getTimezoneOffset() * 60000)).toJSON())
 
     setIsShowAll(false);
     resetFilterState(screenlabels.dataset.geography);
