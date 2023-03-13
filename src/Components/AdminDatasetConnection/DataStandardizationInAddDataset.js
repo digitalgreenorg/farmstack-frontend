b51f2e99c295642dcaafb8584a39cf7a8946bf72
Nilesh Tiwari <nilesh@Nileshs-MacBook-Pro.local>
2023-03-13 23:27:10 +0530

    working on dataset standardization

--- src/Components/AdminDatasetConnection/DataStandardizationInAddDataset.js
+++ src/Components/AdminDatasetConnection/DataStandardizationInAddDataset.js
@@ -3,7 +3,9 @@ import InputLabel from "@mui/material/InputLabel";
 import MenuItem from "@mui/material/MenuItem";
 import FormControl from "@mui/material/FormControl";
 import Select, { SelectChangeEvent } from "@mui/material/Select";
-import './DataStandardizationInAddDataset.css'
+import "./DataStandardizationInAddDataset.css";
+import Checkbox from "@mui/material/Checkbox";
+import { FormControlLabel, FormGroup } from "@mui/material";
 
 const DataStandardizationInAddDataset = () => {
   const [keysInUploadedDataset, setKeysInUploadedDataset] = useState([
@@ -16,6 +18,7 @@ const DataStandardizationInAddDataset = () => {
     { key: "farmer_name", description: "give farmer name" },
     { key: "farmer_name", description: "give farmer name" },
   ]);
+  const [checkBox, setCheckBox] = useState(false);
 
   const handleChange = (e) => {
     console.log("handle change clicked");
@@ -24,44 +27,52 @@ const DataStandardizationInAddDataset = () => {
 
   return (
     <>
-    <div className="data_standardization_title" >
-      <span>
-        Uploaded Data Column Name
-      </span>
-      <span>
-        Standard Data Column Name
-      </span>
-    </div>
+      <div className="data_standardization_title">
+        <span>Uploaded Data Column Name</span>
+        <span>Standard Data Column Name</span>
+      </div>
       {keysInUploadedDataset?.map((keyNames, index) => {
         return (
-          <div className="data_standardization_cloumn_container"
-          >
-           <h3 className="uploaded_data_column_name_title" >{keyNames}</h3>
-            <div>
-            <FormControl sx={{ m: 1, minWidth: 300 }} size="small">
-              <InputLabel id="demo-select-small">Select column/key</InputLabel>
-              <Select
-                labelId="demo-select-small"
-                id="demo-select-small"
-                // value={age}
-                onChange={handleChange}
-              >
-              {
-                standardColumnNames?.map((item,index)=>{
-
-                  return<>
-                  
-                <MenuItem value={item.key}>{item.key}</MenuItem>
-                  </>
-                })
-              }
-              </Select>
-            </FormControl>
+          <>
+            <div className="data_standardization_cloumn_container">
+              <div className="uploaded_data_column_name_title_container">
+                
+                <div className="uploaded_data_column_name_title">
+                  {keyNames}
+                </div>
+              </div>
+              <div>
+                <FormControl sx={{ m: 1, minWidth: 300 }} size="small">
+                  <InputLabel id="demo-select-small">
+                    Select column/key
+                  </InputLabel>
+                  <Select
+                    labelId="demo-select-small"
+                    id="demo-select-small"
+                    // value={age}
+                    onChange={handleChange}
+                  >
+                    {standardColumnNames?.map((item, index) => {
+                      return (
+                        <>
+                          <MenuItem value={item.key}>{item.key}</MenuItem>
+                        </>
+                      );
+                    })}
+                  </Select>
+                </FormControl>
+              </div>
+              <FormGroup>
+                  <FormControlLabel
+                    control={<Checkbox onChange={() => setCheckBox(true)} />}
+                    label="Mask"
+                  />
+                </FormGroup>
             </div>
-          </div>
+            <hr />
+          </>
         );
       })}
-       
     </>
   );
 };
