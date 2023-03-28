01b096487a559d6fe169b2c2314346c2911d5de9
Sohit-dg <sohit@digitalgreen.org>
2023-03-28 13:22:10 +0530

    fs_dev : Basic Details tab component added.

---
+++ src/Components/Datasets/TabComponents/BasicDetails.js
@@ -0,0 +1,256 @@
+import React, { useState } from 'react';
+import { Box, Checkbox, TextField, Typography } from '@mui/material';
+import { DatePicker, LocalizationProvider } from '@mui/x-date-pickers';
+import { AdapterDateFns } from "@mui/x-date-pickers/AdapterDateFns";
+
+const BasicDetails = () => {
+
+    const [dataSetName, setDataSetName] = useState('');
+    const [dataSetDescription, setDataSetDescription] = useState('');
+    const [fromDate, setFromDate] = useState('');
+    const [toDate, setToDate] = useState('');
+    const limitChar = 512;
+
+    const handleDescription = (e) => {
+        if (e.target.value.toString().length <= limitChar) {
+            setDataSetDescription(e.target.value);
+        }
+    };
+
+    const handleFromDate = (value) => {
+        setFromDate(value);
+    }
+
+    const handleToDate = (value) => {
+        setToDate(value);
+    }
+
+    return (
+        <div className='mt-20'>
+            <Typography sx={{
+                fontFamily: "Montserrat !important",
+                fontWeight: "600",
+                fontSize: "32px",
+                lineHeight: "40px",
+                color: "#000000",
+                textAlign: 'left'
+            }}>Add new dataset</Typography>
+            <TextField
+                fullWidth
+                helperText={
+                    <Typography
+                        sx={{
+                            fontFamily: "Montserrat !important",
+                            fontWeight: "400",
+                            fontSize: "12px",
+                            lineHeight: "18px",
+                            color: "#FF0000",
+                            textAlign: 'left'
+                        }}
+                    >
+                        {(dataSetName !== null && dataSetName !== undefined && dataSetName !== '') ? '' : 'Please enter the dataset name is a mandatory field.'}
+                    </Typography>}
+                sx={{
+                    marginTop: '30px',
+                    borderRadius: '8px',
+                    '& .MuiOutlinedInput-root': {
+                        '& fieldset': {
+                            borderColor: '#919EAB'
+                        },
+                        '&:hover fieldset': {
+                            borderColor: '#919EAB'
+                        },
+                        '&.Mui-focused fieldset': {
+                            borderColor: '#919EAB'
+                        },
+                    }
+                }}
+                placeholder='Dataset name'
+                value={dataSetName}
+                onChange={(e) => setDataSetName(e.target.value)}
+            />
+            <TextField
+                fullWidth
+                multiline
+                minRows={4}
+                maxRows={4}
+                sx={{
+                    marginTop: '12px',
+                    borderRadius: '8px',
+                    '& .MuiOutlinedInput-root': {
+                        '& fieldset': {
+                            borderColor: '#919EAB'
+                        },
+                        '&:hover fieldset': {
+                            borderColor: '#919EAB'
+                        },
+                        '&.Mui-focused fieldset': {
+                            borderColor: '#919EAB'
+                        },
+                    }
+                }}
+                placeholder='Dataset description not more that 512 character '
+                value={dataSetDescription}
+                onChange={(e) => handleDescription(e)}
+            />
+            <Typography sx={{
+                fontFamily: "Montserrat !important",
+                fontWeight: "600",
+                fontSize: "32px",
+                lineHeight: "40px",
+                color: "#000000",
+                textAlign: 'left',
+                marginTop: '50px'
+            }}>Data capture interval</Typography>
+            <Box sx={{ display: 'flex', marginTop: '20px' }}>
+                <div>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DatePicker
+                            inputFormat="dd/MM/yyyy"
+                            placeholder="Start Date"
+                            label="Start Date"
+                            value={fromDate}
+                            onChange={(value) => handleFromDate(value)}
+                            PaperProps={{
+                                sx: {
+                                    borderRadius: '16px !important',
+                                    "& .MuiPickersDay-root": {
+                                        "&.Mui-selected": {
+                                            backgroundColor: "#007B55 !important",
+                                        },
+                                    },
+                                }
+                            }}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    id="filled-basic"
+                                    variant="outlined"
+                                    sx={{
+                                        width: '468px',
+                                        svg: { color: '#00AB55' },
+                                        "& .MuiInputBase-input": {
+                                            height: "36px"
+                                        },
+                                        '& .MuiOutlinedInput-root': {
+                                            '& fieldset': {
+                                                borderColor: '#919EAB !important'
+                                            },
+                                            '&:hover fieldset': {
+                                                borderColor: '#919EAB'
+                                            },
+                                            '&.Mui-focused fieldset': {
+                                                borderColor: '#919EAB'
+                                            },
+                                        }
+                                    }}
+                                    helperText={
+                                        <Typography
+                                            sx={{
+                                                fontFamily: "Montserrat !important",
+                                                fontWeight: "400",
+                                                fontSize: "12px",
+                                                lineHeight: "18px",
+                                                color: "#FF0000",
+                                                textAlign: 'left'
+                                            }}
+                                        >
+                                            {(toDate !== null && toDate !== undefined && toDate !== '') ? '' : 'Please enter the start date of the data capture interval.'}
+                                        </Typography>
+                                    }
+                                />
+                            )}
+                        // error={props.dataCaptureStartErrorMessage ? true : false}
+                        />
+                    </LocalizationProvider>
+                </div>
+
+                <div style={{ marginLeft: '24px' }}>
+                    <LocalizationProvider dateAdapter={AdapterDateFns}>
+                        <DatePicker
+                            inputFormat="dd/MM/yyyy"
+                            label="End Date"
+                            value={fromDate}
+                            onChange={(value) => handleToDate(value)}
+                            PaperProps={{
+                                sx: {
+                                    borderRadius: '16px !important',
+                                    "& .MuiPickersDay-root": {
+                                        "&.Mui-selected": {
+                                            backgroundColor: "#007B55 !important",
+                                        },
+                                    },
+                                }
+                            }}
+                            renderInput={(params) => (
+                                <TextField
+                                    {...params}
+                                    id="filled-basic"
+                                    variant="outlined"
+                                    sx={{
+                                        width: '468px',
+                                        svg: { color: '#00AB55' },
+                                        "& .MuiInputBase-input": {
+                                            height: "36px"
+                                        },
+                                        '& .MuiOutlinedInput-root': {
+                                            '& fieldset': {
+                                                borderColor: '#919EAB !important'
+                                            },
+                                            '&:hover fieldset': {
+                                                borderColor: '#919EAB'
+                                            },
+                                            '&.Mui-focused fieldset': {
+                                                borderColor: '#919EAB'
+                                            },
+                                        }
+                                    }}
+                                    helperText={
+                                        <Typography
+                                            sx={{
+                                                fontFamily: "Montserrat !important",
+                                                fontWeight: "400",
+                                                fontSize: "12px",
+                                                lineHeight: "18px",
+                                                color: "#FF0000",
+                                                textAlign: 'left'
+                                            }}
+                                        >
+                                            {(toDate !== null && toDate !== undefined && toDate !== '') ? '' : 'Please enter the end date of the data capture interval.'}
+                                        </Typography>
+                                    }
+                                />
+                            )}
+                        // error={props.dataCaptureStartErrorMessage ? true : false}
+
+                        />
+                    </LocalizationProvider>
+                </div>
+            </Box>
+            <Box sx={{ display: 'flex', alignItems: 'center', marginTop: '4px' }}>
+                <div>
+                    <Checkbox
+                        sx={{
+                            '&.Mui-checked': {
+                                color: '#00AB55 !important',
+                            },
+                        }}
+                    />
+                </div>
+                <Typography
+                    sx={{
+                        fontFamily: "Montserrat !important",
+                        fontWeight: "400",
+                        fontSize: "16px",
+                        lineHeight: "22px",
+                        color: "#212B36",
+                        textAlign: 'left',
+                        marginLeft: '5px'
+                    }}
+                >Constantly updating</Typography>
+            </Box>
+        </div>
+    )
+}
+
+export default BasicDetails
--- src/Views/Role/Participant/Dataset/AddDataSetParticipantNew.js
+++ src/Views/Role/Participant/Dataset/AddDataSetParticipantNew.js
@@ -1,8 +1,10 @@
 import React, { useState } from 'react'
-import { Box, Tab, Tabs } from '@mui/material';
+import { Box, Button, Divider, Tab, Tabs } from '@mui/material';
 import { useHistory } from "react-router-dom";
 import { getTokenLocal } from "../../../../Utils/Common";
 import './AddDataSetParticipantNew.css';
+import FooterNew from '../../../../Components/Footer/FooterNew';
+import BasicDetails from '../../../../Components/Datasets/TabComponents/BasicDetails';
 
 function TabPanel(props) {
     const { children, value, index, ...other } = props;
@@ -15,7 +17,7 @@ function TabPanel(props) {
             aria-labelledby={`simple-tab-${index}`}
             {...other}
         >
-            {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
+            {value === index && <Box>{children}</Box>}
         </div>
     );
 }
@@ -28,51 +30,102 @@ const AddDataSetParticipantNew = () => {
         setValue(newValue);
     };
 
+    const handleNext = () => {
+        if (value >= 0 && value < 4) {
+            setValue(value + 1)
+        }
+    }
+
+    const handleSubmit = () => {
+
+    }
+
     return (
-        <Box sx={{ marginLeft: '144px', marginRight: '144px' }}>
-            <div className='text-left mt-50'>
-                <span className='add_light_text'>Dataset</span>
-                <span className='add_light_text ml-16'>
-                    <img src={require("../../../../Assets/Img/dot.svg")} />
-                </span>
-                <span className='add_light_text ml-16'>Add new dataset</span>
-            </div>
-            <Box sx={{ marginTop: '63px', borderBottom: 1, borderColor: 'divider', borderBottom: '1px solid #3D4A52 !important' }}>
-                <Tabs
-                    className='tabs'
-                    sx={{
-                        '& .MuiTabs-indicator': { backgroundColor: "#00AB55 !important" },
-                        '& .MuiTab-root': {
-                            color: "#637381 !important",
-                            borderLeft: 'none !important',
-                            borderTop: 'none !important',
-                            borderRight: 'none !important',
-                        },
-                        '& .Mui-selected': { color: "#00AB55 !important" },
-                    }}
-                    value={value} onChange={handleChange}>
-                    <Tab label={<span className={value == 0 ? 'tab_header_selected' : 'tab_header'}>Basic details</span>} />
-                    <Tab label={<span className={value == 1 ? 'tab_header_selected' : 'tab_header'}>Upload or import</span>} />
-                    <Tab label={<span className={value == 2 ? 'tab_header_selected' : 'tab_header'}>Standardise</span>} />
-                    <Tab label={<span className={value == 3 ? 'tab_header_selected' : 'tab_header'}>Categorise</span>} />
-                    <Tab label={<span className={value == 4 ? 'tab_header_selected' : 'tab_header'}>Usage policy</span>} />
-                </Tabs>
+        <Box>
+            <Box sx={{ marginLeft: '144px', marginRight: '144px' }}>
+                <div className='text-left mt-50'>
+                    <span className='add_light_text'>Dataset</span>
+                    <span className='add_light_text ml-16'>
+                        <img src={require("../../../../Assets/Img/dot.svg")} />
+                    </span>
+                    <span className='add_light_text ml-16'>Add new dataset</span>
+                </div>
+                <Box sx={{ marginTop: '63px', borderBottom: 1, borderColor: 'divider', borderBottom: '1px solid #3D4A52 !important' }}>
+                    <Tabs
+                        className='tabs'
+                        sx={{
+                            '& .MuiTabs-indicator': { backgroundColor: "#00AB55 !important" },
+                            '& .MuiTab-root': {
+                                color: "#637381 !important",
+                                borderLeft: 'none !important',
+                                borderTop: 'none !important',
+                                borderRight: 'none !important',
+                            },
+                            '& .Mui-selected': { color: "#00AB55 !important" },
+                        }}
+                        value={value} onChange={handleChange}>
+                        <Tab label={<span className={value == 0 ? 'tab_header_selected' : 'tab_header'}>Basic details</span>} />
+                        <Tab label={<span className={value == 1 ? 'tab_header_selected' : 'tab_header'}>Upload or import</span>} />
+                        <Tab label={<span className={value == 2 ? 'tab_header_selected' : 'tab_header'}>Standardise</span>} />
+                        <Tab label={<span className={value == 3 ? 'tab_header_selected' : 'tab_header'}>Categorise</span>} />
+                        <Tab label={<span className={value == 4 ? 'tab_header_selected' : 'tab_header'}>Usage policy</span>} />
+                    </Tabs>
+                </Box>
+                <TabPanel value={value} index={0}>
+                    <BasicDetails />
+                </TabPanel>
+                <TabPanel value={value} index={1}>
+                    Item Two
+                </TabPanel>
+                <TabPanel value={value} index={2}>
+                    Item Three
+                </TabPanel>
+                <TabPanel value={value} index={3}>
+                    Item Four
+                </TabPanel>
+                <TabPanel value={value} index={4}>
+                    Item Five
+                </TabPanel>
+                <Divider sx={{ border: '1px solid #ABABAB', marginTop: '59px' }} />
+                <Box className='d-flex justify-content-end' sx={{ marginTop: '50px', marginBottom: '100px' }}>
+                    <Button
+                        sx={{
+                            fontFamily: 'Montserrat',
+                            fontWeight: 700,
+                            fontSize: '16px',
+                            width: "171px",
+                            height: "48px",
+                            border: "1px solid rgba(0, 171, 85, 0.48)",
+                            borderRadius: "8px",
+                            color: "#00AB55",
+                            textTransform: 'none',
+                            '&:hover': {
+                                background: 'none',
+                                border: "1px solid rgba(0, 171, 85, 0.48)"
+                            }
+                        }}
+                        variant='outlined' onClick={() => history.push('/participant/new_datasets')}>Cancel</Button>
+                    <Button
+                        sx={{
+                            fontFamily: 'Montserrat',
+                            fontWeight: 700,
+                            fontSize: '16px',
+                            width: "171px",
+                            height: "48px",
+                            background: "#00AB55",
+                            borderRadius: "8px",
+                            textTransform: 'none',
+                            marginLeft: '50px',
+                            '&:hover': {
+                                backgroundColor: '#00AB55',
+                                color: '#fffff',
+                            }
+                        }}
+                        variant='contained' onClick={() => { value === 4 ? handleSubmit() : handleNext() }}>{value === 4 ? 'Submit' : 'Next'}</Button>
+                </Box>
             </Box>
-            <TabPanel value={value} index={0}>
-                Item One
-            </TabPanel>
-            <TabPanel value={value} index={1}>
-                Item Two
-            </TabPanel>
-            <TabPanel value={value} index={2}>
-                Item Three
-            </TabPanel>
-            <TabPanel value={value} index={3}>
-                Item Four
-            </TabPanel>
-            <TabPanel value={value} index={4}>
-                Item Five
-            </TabPanel>
+            <Divider />
+            <FooterNew />
         </Box>
     )
 }
