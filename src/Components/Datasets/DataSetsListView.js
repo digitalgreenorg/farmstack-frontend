c6756991a74bf0a9bc3806d72cf03acbf38da208
Sohit-dg <sohit@digitalgreen.org>
2023-04-04 15:15:04 +0530

    fs_dev : Category in list view  implemented.

--- src/Components/Datasets/DataSetsListView.js
+++ src/Components/Datasets/DataSetsListView.js
@@ -20,7 +20,11 @@ const DataSetsListView = ({ datasets, history }) => {
                     <Box className='d-flex justify-content-between mb-20 mt-20 cursor-pointer' onClick={() => history.push(`/participant/new_datasets/view/${item.id}`)}>
                         <Typography className='datasets_list_view_text datasets_list_view_name green_text w-100 text-left ml-20'>{item?.name}</Typography>
                         <Typography className='datasets_list_view_text w-100 text-left ml-90'>{item?.organization?.name}</Typography>
-                        <Typography className='datasets_list_view_text w-100 text-left'>{"Wheat"}</Typography>
+                        <Typography className='datasets_list_view_text w-100 text-left'>
+                            {(Object.keys(item?.category).length) ?
+                                (Object.keys(item?.category).length > 1 ? Object.keys(item?.category)[0] + " +" + String(Object.keys(item?.category).length - 1) : Object.keys(item?.category)[0])
+                                : 'NA'}
+                        </Typography>
                         <Typography className='datasets_list_view_text w-100 text-left'>{item?.geography}</Typography>
                         <Typography className='datasets_list_view_text w-100 text-left'>{item?.age_of_date ? item.age_of_date : 'NA'}</Typography>
                         <Typography className='datasets_list_view_text w-100 text-center'>{dateTimeFormat(item?.created_at, false)}</Typography>
