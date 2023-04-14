b2030545fe5a49819e3f44ae42d14a33c8315c88
Sohit-dg <sohit@digitalgreen.org>
2023-04-14 13:29:36 +0530

    hardcoded dataset id removed from usagepolicy.

--- src/Components/Datasets_New/TabComponents/UsagePolicy.js
+++ src/Components/Datasets_New/TabComponents/UsagePolicy.js
@@ -24,7 +24,7 @@ const UsagePolicy = (props) => {
     };
 
     const getAllFileNames = () => {
-        let url = UrlConstant.base_url + UrlConstant.list_of_files + datasetId;
+        let url = UrlConstant.base_url + UrlConstant.list_of_files + props.datasetId;
         let accessToken = getTokenLocal() ?? false;
         callLoader(true)
         HTTPService("GET", url, false, false, accessToken)
