cacae249f5bd32d4c41d2f880b884293759e5dd4
Sohit-dg <sohit@digitalgreen.org>
2023-05-03 18:56:25 +0530

    feat : navbar, footer, datasets page responsiveness.

--- src/Components/Footer/Footer_New.jsx
+++ src/Components/Footer/Footer_New.jsx
@@ -1,5 +1,12 @@
 import React, { useEffect, useState } from "react";
-import { Box, Button, InputAdornment, TextField } from "@mui/material";
+import {
+  Box,
+  Button,
+  Divider,
+  InputAdornment,
+  TextField,
+  useTheme,
+} from "@mui/material";
 import style from "./Footer_New.module.css";
 import ContainedButton from "../Button/ContainedButton";
 import { useHistory } from "react-router-dom";
@@ -11,12 +18,22 @@ import {
   isLoggedInUserCoSteward,
   isLoggedInUserParticipant,
 } from "../../Utils/Common";
-
+import useMediaQuery from "@mui/material/useMediaQuery";
 const FooterNew = () => {
   const handleSubscribe = () => {};
   const history = useHistory();
   const [adminData, setAdminData] = useState(null);
+  const theme = useTheme();
+  const mobile = useMediaQuery(theme.breakpoints.down("sm"));
+  const tablet = useMediaQuery(theme.breakpoints.down("md"));
+  const miniLaptop = useMediaQuery(theme.breakpoints.down("lg"));
 
+  const containerStyle = {
+    padding: mobile || tablet ? "" : "40px",
+    paddingTop: mobile || tablet ? "40px" : "",
+    marginLeft: mobile || tablet ? "30px" : "144px",
+    marginRight: mobile || tablet ? "30px" : "144px",
+  };
   const handleItemClick = (name) => {
     if (name === "datasets") {
       if (isLoggedInUserAdmin() || isLoggedInUserCoSteward()) {
@@ -49,179 +66,250 @@ const FooterNew = () => {
       .catch((error) => {});
   }, []);
   return (
-    <Box sx={{ padding: "40px", marginLeft: "144px", marginRight: "144px" }}>
-      <div className="logo_container text-left">
-        <img
-          style={{ height: "auto", width: "172px", maxHeight: "65px" }}
-          src={
-            UrlConstant.base_url_without_slash + adminData?.organization?.logo
-          }
-          alt="footerLogo"
-        />
-      </div>
-      <div className={` ${style.footerContent} text-left`}>
-        <div className={`contact`}>
-          <div className={`${style.footerTitle}`}>Contacts</div>
-          <div className="mb-30 mt-20">
-            <div className={`${style.footerLightTextOnly} text-left`}>
-              Email
-            </div>
-            <div className={`${style.footerDarkText} mt-2 text-left`}>
-              {adminData?.user?.email ?? ""}
-            </div>
-          </div>
-          <div className="mb-30">
-            <div className={`${style.footerLightTextOnly} text-left`}>
-              Datahub admin phone
-            </div>
-            <div className={`${style.footerDarkText} mt-2 text-left`}>
-              {adminData?.user?.phone_number ?? ""}
-            </div>
-          </div>
-          <div>
-            <div className={`${style.footerLightTextOnly} text-left`}>
-              Organization Website
-            </div>
-            <div className={`${style.link} mt-2`}>
-              {" "}
-              {adminData?.organization?.website ?? ""}
-            </div>
-          </div>
+    <>
+      <Divider className="mt-50" />
+      <Box sx={containerStyle}>
+        <div className="logo_container text-left">
+          <img
+            style={{
+              height: "auto",
+              width: "172px",
+              maxHeight: "65px",
+              boxShadow: "rgba(100, 100, 111, 0.2) 0px 7px 29px 0px",
+            }}
+            src={
+              UrlConstant.base_url_without_slash + adminData?.organization?.logo
+            }
+            alt="footerLogo"
+          />
         </div>
-        <div className={`links w-25`}>
-          <div className={`${style.footerTitle}`}>Quick links</div>
-          <div className={`footer_link mt-20`}>
-            <div className="d-flex justify-content-between w-100">
-              <div
-                className={`${style.footerLightText} ${style.quickLinks}`}
-                onClick={() => {
-                  history.push("/home");
-                  goToTop(0);
-                }}
-              >
-                Home
-              </div>
-
-              <div
-                onClick={() => history.push("/home/contact")}
-                className={`${style.footerLightText} ${style.flexWidth} ${style.quickLinks}`}
-              >
-                Contact us
-              </div>
-            </div>
-            <div className="d-flex justify-content-between w-100">
-              <div
-                className={`${style.footerLightText} ${style.quickLinks} mt-10`}
-              >
-                About {adminData?.organization?.name ?? ""}
-              </div>
-              <div
-                className={`${style.footerLightText} ${style.quickLinks} ${style.flexWidth} mt-10`}
-                onClick={() => history.push("/home/legal")}
-              >
-                Legal
-              </div>
-            </div>
-            <div className="d-flex justify-content-between w-100">
-              <div
-                className={`${style.footerLightText} ${style.quickLinks} mt-10`}
-                onClick={() => window.open("https://farmstack.co/")}
-              >
-                About Farmstack
-              </div>
-
-              {isLoggedInUserAdmin() ||
-              isLoggedInUserCoSteward() ||
-              isLoggedInUserParticipant() ? (
-                ""
-              ) : (
-                <div
-                  className={`${style.footerLightText} ${style.quickLinks} ${style.flexWidth} mt-10`}
-                  onClick={() => history.push("/login")}
-                >
-                  Login
+        <div
+          className={` ${
+            mobile
+              ? style.footerContentMd
+              : tablet
+              ? style.footerContent
+              : miniLaptop
+              ? style.footerContentMd
+              : style.footerContent
+          } text-left`}
+        >
+          <Box className="d-flex justify-content-between">
+            <div className={`contact`}>
+              <div className={`${style.footerTitle}`}>Contacts</div>
+              <div className="mb-30 mt-20">
+                <div className={`${style.footerLightTextOnly} text-left`}>
+                  Email
                 </div>
-              )}
-            </div>
-            <div className="d-flex justify-content-between w-100">
-              <div
-                className={`${style.footerLightText} ${style.quickLinks} mt-10 `}
-                onClick={() => handleItemClick("datasets")}
-              >
-                Datasets
-              </div>
-
-              {isLoggedInUserAdmin() ||
-              isLoggedInUserCoSteward() ||
-              isLoggedInUserParticipant() ? (
-                ""
-              ) : (
-                <div
-                  className={`${style.footerLightText} ${style.quickLinks} mt-10`}
-                  onClick={() => history.push("/login")}
-                >
-                  Get started
+                <div className={`${style.footerDarkText} mt-2 text-left`}>
+                  {adminData?.user?.email ?? ""}
                 </div>
-              )}
-            </div>
-            <div className="d-flex justify-content-between w-100">
-              <div
-                className={`${style.footerLightText} ${style.quickLinks} mt-10 `}
-                onClick={() => handleItemClick("participants")}
-              >
-                Participants
+              </div>
+              <div className="mb-30">
+                <div className={`${style.footerLightTextOnly} text-left`}>
+                  Datahub admin phone
+                </div>
+                <div className={`${style.footerDarkText} mt-2 text-left`}>
+                  {adminData?.user?.phone_number ?? ""}
+                </div>
+              </div>
+              <div>
+                <div className={`${style.footerLightTextOnly} text-left`}>
+                  Organization Website
+                </div>
+                <div className={`${style.link} mt-2`}>
+                  {" "}
+                  {adminData?.organization?.website ?? ""}
+                </div>
               </div>
             </div>
-          </div>
+            <div
+              className={`links ${mobile || tablet ? "w-100" : style.w268} ${
+                mobile || tablet ? style.ml63 : style.ml50
+              }`}
+            >
+              <div className={`${style.footerTitle}`}>Quick links</div>
+              <div className={`footer_link mt-20`}>
+                <div
+                  className={`${
+                    mobile || tablet
+                      ? "d-block"
+                      : "d-flex justify-content-between"
+                  } w-100`}
+                >
+                  <div
+                    className={`${style.footerLightText} ${style.quickLinks}`}
+                    onClick={() => {
+                      history.push("/home");
+                      goToTop(0);
+                    }}
+                  >
+                    Home
+                  </div>
+                  <div
+                    onClick={() => history.push("/home/contact")}
+                    className={`${style.footerLightText} ${style.flexWidth} ${
+                      style.quickLinks
+                    } ${mobile || tablet ? "mt-10" : ""} `}
+                  >
+                    Contact us
+                  </div>
+                </div>
+                <div
+                  className={`${
+                    mobile || tablet
+                      ? "d-block"
+                      : "d-flex justify-content-between"
+                  } w-100`}
+                >
+                  <div
+                    className={`${style.footerLightText} ${style.quickLinks} mt-10`}
+                  >
+                    About {adminData?.organization?.name ?? ""}
+                  </div>
+                  <div
+                    className={`${style.footerLightText} ${style.quickLinks} ${style.flexWidth} mt-10`}
+                    onClick={() => history.push("/home/legal")}
+                  >
+                    Legal
+                  </div>
+                </div>
+                <div
+                  className={`${
+                    mobile || tablet
+                      ? "d-block"
+                      : "d-flex justify-content-between"
+                  } w-100`}
+                >
+                  <div
+                    className={`${style.footerLightText} ${style.quickLinks} mt-10`}
+                    onClick={() => window.open("https://farmstack.co/")}
+                  >
+                    About Farmstack
+                  </div>
+
+                  {isLoggedInUserAdmin() ||
+                  isLoggedInUserCoSteward() ||
+                  isLoggedInUserParticipant() ? (
+                    ""
+                  ) : (
+                    <div
+                      className={`${style.footerLightText} ${style.quickLinks} ${style.flexWidth} mt-10`}
+                      onClick={() => history.push("/login")}
+                    >
+                      Login
+                    </div>
+                  )}
+                </div>
+                <div
+                  className={`${
+                    mobile || tablet
+                      ? "d-block"
+                      : "d-flex justify-content-between"
+                  } w-100`}
+                >
+                  <div
+                    className={`${style.footerLightText} ${style.quickLinks} mt-10 `}
+                    onClick={() => handleItemClick("datasets")}
+                  >
+                    Datasets
+                  </div>
+
+                  {isLoggedInUserAdmin() ||
+                  isLoggedInUserCoSteward() ||
+                  isLoggedInUserParticipant() ? (
+                    ""
+                  ) : (
+                    <div
+                      className={`${style.footerLightText} ${style.quickLinks} mt-10`}
+                      onClick={() => history.push("/login")}
+                    >
+                      Get started
+                    </div>
+                  )}
+                </div>
+                <div
+                  className={`${
+                    mobile || tablet
+                      ? "d-block"
+                      : "d-flex justify-content-between"
+                  } w-100`}
+                >
+                  <div
+                    className={`${style.footerLightText} ${style.quickLinks} mt-10 `}
+                    onClick={() => handleItemClick("participants")}
+                  >
+                    Participants
+                  </div>
+                </div>
+              </div>
+            </div>
+          </Box>
+          <Box>
+            <div
+              className={`staytuned ${
+                mobile || tablet || miniLaptop ? "mt-20" : ""
+              }`}
+            >
+              <div className={`${style.footerTitle}`}>Stay tuned</div>
+              <div className={`mt-20 ${style.footerLightText}`}>
+                Subscribe to our newsletter and never miss datasets,
+              </div>
+              <div className={`${style.footerLightText} mb-30`}>
+                latest news, etc.,
+              </div>
+              <div className={`${style.footerLightText}`}>
+                Our newsletter is sent once a month every first week.
+              </div>
+              <div className="mt-20 mb-20">
+                <TextField
+                  sx={{
+                    "& .MuiOutlinedInput-root": {
+                      "& fieldset": {
+                        borderColor: "#00AB55",
+                      },
+                      "&:hover fieldset": {
+                        borderColor: "#00AB55",
+                      },
+                      "&.Mui-focused fieldset": {
+                        borderColor: "#00AB55",
+                      },
+                    },
+                  }}
+                  className={
+                    mobile
+                      ? "input_field_subscribe_sm"
+                      : tablet
+                      ? "input_field_subscribe_md"
+                      : "input_field_subscribe"
+                  }
+                  placeholder="Enter your e-mail id"
+                  variant="outlined"
+                  inputProps={{
+                    style: { height: "30px" },
+                  }}
+                  InputProps={{
+                    endAdornment: (
+                      <InputAdornment position="end">
+                        <ContainedButton
+                          text={"Subscribe"}
+                          fontWeight={"700"}
+                          fontFamily={"Public Sans"}
+                          fontSize={mobile || tablet ? "10px" : "16px"}
+                          width={mobile ? "83px" : tablet ? "83px" : "172px"}
+                          height={mobile ? "30px" : tablet ? "30px" : "61px"}
+                          handleClick={handleSubscribe}
+                        />
+                      </InputAdornment>
+                    ),
+                  }}
+                />
+              </div>
+            </div>
+          </Box>
         </div>
-        <div className={`staytuned`}>
-          <div className={`${style.footerTitle}`}>Stay tuned</div>
-          <div className={`mt-20 ${style.footerLightText}`}>
-            Subscribe to our newsletter and never miss datasets,
-          </div>
-          <div className={`${style.footerLightText} mb-30`}>
-            latest news, etc.,
-          </div>
-          <div className={`${style.footerLightText}`}>
-            Our newsletter is sent once a month every first week.
-          </div>
-          <div className="mt-20 mb-20">
-            <TextField
-              sx={{
-                "& .MuiOutlinedInput-root": {
-                  "& fieldset": {
-                    borderColor: "#00AB55",
-                  },
-                  "&:hover fieldset": {
-                    borderColor: "#00AB55",
-                  },
-                  "&.Mui-focused fieldset": {
-                    borderColor: "#00AB55",
-                  },
-                },
-              }}
-              className={"input_field_subscribe"}
-              placeholder="Enter your e-mail id"
-              variant="outlined"
-              InputProps={{
-                endAdornment: (
-                  <InputAdornment position="end">
-                    <ContainedButton
-                      text={"Subscribe"}
-                      fontWeight={"700"}
-                      fontSize={"16px"}
-                      width={"172px"}
-                      height={"56px"}
-                      handleClick={handleSubscribe}
-                    />
-                  </InputAdornment>
-                ),
-              }}
-            />
-          </div>
-        </div>
-      </div>
-    </Box>
+      </Box>
+    </>
   );
 };
 
