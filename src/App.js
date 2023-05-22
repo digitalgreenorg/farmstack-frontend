5c2406d532400c519a90162ed9233ffb543f6caf
Nilesh1409 <nileshtiwari7054@gmail.com>
2023-05-22 15:44:46 +0530

    revert changes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect, useco, useState } from "react";
+import React, { useContext, useEffect } from "react";
 import "mdbreact/dist/css/mdb.css";
 import "@fortawesome/fontawesome-free/css/all.min.css";
 import "bootstrap-css-only/css/bootstrap.min.css";
@@ -46,25 +46,17 @@ import {
   isLoggedInUserAdmin,
 } from "./Utils/Common";
 function App() {
-  const { isLoading, toastDetail, setAdminData, callLoader } =
-    useContext(FarmStackContext);
-  const [isOnboarded, setIsOnboarded] = useState(true);
-
+  const { isLoading, toastDetail, setAdminData } = useContext(FarmStackContext);
   function getAdminData() {
     let url =
       UrlConstant.base_url + UrlConstant.microsite_admin_organization + "/";
     let method = "GET";
     // let url = UrlConstant.base_url + UrlConstant.microsite_admin_organization
-    callLoader(true);
     HTTPService(method, url, "", false, false, false, false, false)
       .then((response) => {
-        callLoader(false);
         setAdminData(response.data);
-        let isOnboarded = response.data?.organization?.logo ? true : false;
-        setIsOnboarded(isOnboarded);
       })
       .catch((error) => {
-        callLoader(false);
         console.log("error");
       });
   }
@@ -101,32 +93,6 @@ function App() {
         console.log("error to verify local data", err);
       });
   };
-
-  // function getAdminData() {
-  //   callLoader(true);
-  //   let url =
-  //     UrlConstant.base_url + UrlConstant.microsite_admin_organization + "/";
-  //   let method = "GET";
-  //   // let url = UrlConstant.base_url + UrlConstant.microsite_admin_organization
-  //   HTTPService(method, url, "", false, false, false, false, false)
-  //     .then((response) => {
-  //       callLoader(false);
-
-  //       setAdminData(response.data);
-  //       // if (response.data?.organization?.logo) {
-
-  //       // setActiveStep(stepNumber);
-  //       // }
-  //     })
-  //     .catch((error) => {
-  //       callLoader(false);
-
-  //       console.log("error");
-  //     });
-  // }
-
-  console.log("isonboarded", isOnboarded);
-
   useEffect(() => {
     // verifyUserDataOfLocal();
     getAdminData();
@@ -149,35 +115,21 @@ function App() {
         <Route  path="/login/profile" component={ProfileScreen} /> */}
           {/* <Route exact path="/datahub/login" component={Login} /> */}
           {/* <Route exact path="/participant/login" component={Login} /> */}
-
           <Route exact path="/login" component={OnBoarding} />
           <Route path="/datahub" component={Datahub} />
           <Route path="/participant" component={Participant} />
           <Route path="/sessionexpired" component={SessionExpired} />
           <Route path="/error/:status" component={NewError} />
-
+          <Route path="/home" component={GuestRoutes} />
+          {/* <Route exact path="/home/viewdataset/:id" component={Viewdetails} /> */}
+          <Route exact path="/legal" component={GuestUserLegal} />
+          <Route exact path="/contact" component={GuestUserContactNew} />
           <Route
             exact
             path="/participantregistration"
             component={AddParticipantRegistrationForm}
           />
-          {!isOnboarded ? (
-            <>
-              <Redirect from="/" to="/login" />
-              <Redirect from="*" to="/login" />
-              {/* <Redirect from="/home" to="/login" />
-              <Redirect from="/home/legal" to="/login" /> */}
-              {/* <Redirect from="/home/contact" to="/login" /> */}
-            </>
-          ) : (
-            <>
-              <Route path="/home" component={GuestRoutes} />
-              {/* <Route exact path="/home/viewdataset/:id" component={Viewdetails} /> */}
-              <Route exact path="/legal" component={GuestUserLegal} />
-              <Route exact path="/contact" component={GuestUserContactNew} />
-              <Redirect from="/" to="/home" />
-            </>
-          )}
+          <Redirect from="/" to="/home" />
         </Switch>
       </Router>
     </React.Fragment>
