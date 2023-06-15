e6938f055f47c8781bb633e07af208eca9550a26
ShruthiMonika <shruthimonika@ShruthiMonikas-MacBook-Air.local>
2023-06-15 18:57:55 +0530

    no conflicts

--- src/Components/Support_New/Support.jsx
+++ src/Components/Support_New/Support.jsx
@@ -245,28 +245,28 @@ export default function Support(props) {
     console.log("filter by category is happening");
     let url = UrlConstants.base_url + UrlConstants.support_ticket_tab;
     let data = {};
-      if (isLoggedInUserAdmin()) {
-        if (tabValue == 0) {
-          data["category"] = e.target.value;
-          data["others"] = false;
-        } else if (tabValue == 1) {
-          data["category"] = e.target.value;
-          data["others"] = true;
-        }
-      } else if (isLoggedInUserCoSteward()) {
-        if (tabValue == 0) {
-          data["category"] = e.target.value;
-          data["others"] = false;
-        } else if (tabValue == 1) {
-          data["category"] = e.target.value;
-          data["others"] = true;
-        }
-      } else {
+    if (isLoggedInUserAdmin()) {
+      if (tabValue == 0) {
         data["category"] = e.target.value;
-      }
-      if (isLoadMore) {
+        data["others"] = false;
+      } else if (tabValue == 1) {
         data["category"] = e.target.value;
+        data["others"] = true;
       }
+    } else if (isLoggedInUserCoSteward()) {
+      if (tabValue == 0) {
+        data["category"] = e.target.value;
+        data["others"] = false;
+      } else if (tabValue == 1) {
+        data["category"] = e.target.value;
+        data["others"] = true;
+      }
+    } else {
+      data["category"] = e.target.value;
+    }
+    if (isLoadMore) {
+      data["category"] = e.target.value;
+    }
     HTTPService("POST", url, data, false, true)
       .then((response) => {
         callLoader(false);
@@ -652,7 +652,7 @@ export default function Support(props) {
                 setCategoryFilter("");
                 setStatusFilter("");
                 setShowFilter(false);
-                setSearchTickets("")
+                setSearchTickets("");
                 getListOfTickets();
               }}
               id="dataset-filter-clear-all-id"
