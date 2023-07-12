223592ce21091fd13a607ed8686a02a60fb59805
ShruthiMonika <shruthimonika@192.168.1.15>
2023-07-12 14:51:01 +0530

    FSPROD: 1732, 1806

--- src/Components/Participants/ParticipantsCarouselNew.js
+++ src/Components/Participants/ParticipantsCarouselNew.js
@@ -1,8 +1,6 @@
-import React, { Component, useEffect, useContext, useState } from "react";
-import { Col, Row } from "react-bootstrap";
+import React, {useEffect, useContext, useState } from "react";
+import {Container} from "react-bootstrap";
 import Slider from "react-slick";
-import "slick-carousel/slick/slick.css";
-import "slick-carousel/slick/slick-theme.css";
 import UrlConstant from "../../Constants/UrlConstants";
 import { useHistory } from "react-router-dom";
 import { FarmStackContext } from "../Contexts/FarmStackContext";
@@ -18,96 +16,20 @@ const ParticipantsCarouselNew = (props) => {
   const { isCosteward } = props;
   const [participantsList, setParticipantsList] = useState([]);
   const settings = {
-    dots: true,
     infinite: true,
-    speed: 500,
-    // slidesToShow: participantsList?.length >= 3 ? 3 : participantsList?.length,
-    // slidesToScroll:
-    //   participantsList?.length >= 3 ? 3 : participantsList?.length,
-    // autoplay: true,
-    className: LocalStyle.slides,
-    responsive: [
-      {
-        breakpoint: 3060,
-        settings: {
-          slidesToShow:
-            participantsList.length >= 4 ? 4 : participantsList.length,
-          slidesToScroll: 3,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 2560,
-        settings: {
-          slidesToShow:
-            participantsList.length >= 4 ? 4 : participantsList.length,
-          slidesToScroll: 3,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 1920,
-        settings: {
-          slidesToShow:
-            participantsList.length >= 4 ? 4 : participantsList.length,
-          slidesToScroll: 3,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 1440,
-        settings: {
-          slidesToShow:
-            participantsList.length >= 3 ? 3 : participantsList.length,
-          slidesToScroll: 2,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 1200,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 1024,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 768,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 1,
-          arrows: false,
-        },
-      },
-      {
-        breakpoint: 480,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          arrows: false,
-        },
-      },
-    ],
+    speed: 200,
+    slidesToShow: 3,
+    autoplay: true,
+    autoplaySpeed: 5000
   };
   let title = isCosteward ? "Co-steward" : "Participants";
   const history = useHistory();
   const { callLoader, callToast, isLoading } = useContext(FarmStackContext);
-  // const [loadMoreUrl, setLoadMoreUrl] = useState("");
-
   const getCoStewardOrParticipantsOnLoad = (
     unApprovedId,
     approval_endpoint
   ) => {
     console.log("in getCoStewardOrParticipantsOnLoad");
-    // setIsLoading(true);
     callLoader(true);
 
     let url =
@@ -120,28 +42,11 @@ const ParticipantsCarouselNew = (props) => {
     HTTPService("GET", url, params, false, false)
       .then((response) => {
         callLoader(false);
-        // if (response?.data?.next == null) {
-        //   setLoadMoreButton(false);
-        // } else {
-        //   setLoadMoreButton(true);
-        //   if (response?.data?.next) setLoadMoreUrl(response.data.next);
-        // }
-        // let tmpData = response.data.results;
         console.log();
         if (response?.data?.results) setParticipantsList(response.data.results);
       })
       .catch(async (e) => {
         callLoader(false);
-        // let error = await GetErrorHandlingRoute(e);
-        // console.log("Error obj", error);
-        // if (error) {
-        //   callToast(
-        //     error?.message,
-        //     error?.status === 200 ? "success" : "error",
-        //     true
-        //   );
-        //   console.log(e);
-        // }
         let error = await GetErrorHandlingRoute(e);
         console.log("Error obj", error);
         console.log(e);
@@ -164,9 +69,7 @@ const ParticipantsCarouselNew = (props) => {
   }, []);
 
   return (
-    // <Row>
-    <div>
-      {/* <h2> Multiple items </h2> */}
+    <Container className="carousel_in_guest_user_main_page">
       {participantsList.length === 0 && !isLoading ? (
         <Box p={3}>
           <NoData
@@ -174,20 +77,12 @@ const ParticipantsCarouselNew = (props) => {
             subTitle={
               "As of now there is no participant, so add participants or invite participants."
             }
-            // primaryButton={"Add participant"}
-            // primaryButtonOnClick={() =>
-            //   history.push("/datahub/participants/add")
-            // }
-            // secondaryButton={"+ Invite participants"}
-            // secondaryButtonOnClick={() =>
-            //   history.push("/datahub/participants/inviteparticipants")
-            // }
           />
         </Box>
       ) : (
         ""
       )}
-      <Slider {...settings}>
+      <Slider className="center" {...settings}>
         {participantsList.length === 0 && !isLoading
           ? ""
           : participantsList?.map((participant, index) => {
@@ -233,8 +128,7 @@ const ParticipantsCarouselNew = (props) => {
               );
             })}
       </Slider>
-    </div>
-    // </Row>
+    </Container>
   );
 };
 
