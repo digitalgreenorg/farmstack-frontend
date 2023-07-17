ac6967c937cf03cf49b176705e2f69fd87a66e3f
kanhaiya suthar <kanhaiyasuthar0@gmail.com>
2023-07-17 10:56:31 +0530

    written test cases for user profile

--- src/tests/onboarding_tests/userprofile.test.jsx
+++ src/tests/onboarding_tests/userprofile.test.jsx
@@ -1,37 +1,22 @@
-import React, { useContext } from "react";
-import { fireEvent, render, screen } from "@testing-library/react";
+import React from "react";
+import { fireEvent, render, screen, waitFor } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import ProfileDetails from "../../Components/NewOnboarding/ProfileDetails";
-import OnBoarding from "../../Views/Pages/HomeScreen/OnBoarding";
-import FarmStackProvider, {
-  FarmStackContext,
-} from "../../Components/Contexts/FarmStackContext";
-import axios from "axios";
-import MockAdapter from "axios-mock-adapter";
+import FarmStackProvider from "../../Components/Contexts/FarmStackContext";
+import { act } from "react-dom/test-utils";
+import { getUserLocal, setUserId } from "../../Utils/Common";
 import UrlConstant from "../../Constants/UrlConstants";
 
+// jest.mock("axios");x
+const userId = "0f76cb90-2394-499b-9b60-bf4cad3751a4";
 describe("User profile", () => {
-  beforeEach(() =>
-    render(
-      <FarmStackProvider>
-        <ProfileDetails />
-      </FarmStackProvider>
-    )
-  );
+  // beforeEach(() => render(<ProfileDetails />, { wrapper: FarmStackProvider }));
   test("rendered correctly", () => {
-    // render(
-    //   <FarmStackProvider>
-    //     <ProfileDetails />
-    //   </FarmStackProvider>
-    // );
+    render(<ProfileDetails />, { wrapper: FarmStackProvider });
   });
 
   test("input existance and event triggering", () => {
-    // render(
-    //   <FarmStackProvider>
-    //     <ProfileDetails />
-    //   </FarmStackProvider>
-    // );
+    render(<ProfileDetails />, { wrapper: FarmStackProvider });
     //checking the element existence and onchange event
     const firstNameElement = screen.getByPlaceholderText("First Name");
     expect(firstNameElement).toBeInTheDocument();
@@ -56,5 +41,28 @@ describe("User profile", () => {
       target: { value: "91 9812301231" },
     });
     expect(contactNumberElement.value).toBe("+91 98123-01231");
+  });
+
+  test("email field is disabled", async () => {
+    render(<ProfileDetails />, { wrapper: FarmStackProvider });
+    const emailElement = screen.getByPlaceholderText(/Enter mail id/i);
+    expect(emailElement).toBeDisabled();
+  });
+
+  test("value", async () => {
+    setUserId("sometoken");
+    const token = getUserLocal();
+    let url = UrlConstant;
+    render(<ProfileDetails />, { wrapper: FarmStackProvider });
+    // act(async () => {
+    let firstName = await screen.findByRole("textbox", { name: /first name/i });
+    expect(firstName.value).toBe("digital");
+    let lastName = await screen.findByRole("textbox", { name: /last name/i });
+    expect(lastName.value).toBe("green");
+    // let emailId = await screen.findByRole("textbox", { name: //i });
+    // expect(lastName.value).toBe("green");
+    // });
+    // screen.debug();
+    expect(true);
   });
 });
