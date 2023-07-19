4facbe6938a78b7587b2cd33d581b5dce11b0d34
kanhaiya suthar <kanhaiyasuthar0@gmail.com>
2023-07-19 10:52:17 +0530

    ADD FSPROD-1763 added test file for orgnaisation detail

--- src/tests/onboarding_tests/organisationDetails.test.js
+++ src/tests/onboarding_tests/organisationDetails.test.js
@@ -1,4 +1,4 @@
-import { rest } from "msw";
+import React from "react";
 import { server } from "../../mocks/server";
 import { cleanup, fireEvent, render, screen } from "@testing-library/react";
 import "@testing-library/jest-dom";
@@ -6,15 +6,24 @@ import { rest } from "msw";
 import { setUserId } from "../../Utils/Common";
 import FarmStackProvider from "../../Components/Contexts/FarmStackContext";
 import OrganizationDetails from "../../Components/NewOnboarding/OrganizationDetails";
-
+import { BrowserRouter as Router } from "react-router-dom";
 afterEach(() => {
   cleanup();
 });
+beforeEach(() => {
+  // cleanup();
+});
 
-describe("Positive scenerio for organisation details in setting and onboarding", () => {
+describe("Positive scenerio for organisation details in setting and onboarding", async () => {
+  console.log("something");
   test("Component rendered successfully", () => {
-    render(<OrganizationDetails />, {
-      wrapper: FarmStackProvider,
-    });
+    render(
+      <Router>
+        <OrganizationDetails />
+      </Router>,
+      {
+        wrapper: FarmStackProvider,
+      }
+    );
   });
 });
