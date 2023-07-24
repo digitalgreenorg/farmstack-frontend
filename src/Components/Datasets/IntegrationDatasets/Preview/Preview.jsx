7b38d911024a5aa7821c929c5d2a8707c5795f9d
kanhaiya suthar <kanhaiyasuthar0@gmail.com>
2023-07-24 14:21:15 +0530

    added renaming feature

--- src/Components/Datasets/IntegrationDatasets/Preview/Preview.jsx
+++ src/Components/Datasets/IntegrationDatasets/Preview/Preview.jsx
@@ -2,12 +2,17 @@ import { Box, Button, Stack, useMediaQuery, useTheme } from "@mui/material";
 import React, { useEffect, useState } from "react";
 import { Col, Row } from "react-bootstrap";
 import styles from "../dataset_integration.module.css";
+import globalStyle from "../../../../Assets/CSS/global.module.css";
 import download_data from "../../../../Assets/Img/download_data.svg";
 import { DataGrid } from "@mui/x-data-grid";
+
 import NoDataAvailable from "../../../Dashboard/NoDataAvailable/NoDataAvailable";
 import { message } from "antd";
 import { useHistory } from "react-router-dom";
 import CustomDeletePopper from "../../../DeletePopper/CustomDeletePopper";
+import ControllerModal from "../../../Generic/ControllerModal.jsx";
+import ModalBody from "./ModalBody";
+import { findType } from "../../../../Utils/Common";
 
 function NoResultsOverlay() {
   return (
@@ -32,6 +37,7 @@ const Preview = (props) => {
     temporaryDeletedCards,
     noOfRecords,
     isConditionForConnectorDataForSaveMet,
+    setIsConditionForConnectorDataForSaveMet,
     isAllConditionForSaveMet,
     connectorData,
     generateData,
@@ -42,6 +48,10 @@ const Preview = (props) => {
     resetAll,
     finalDatasetAfterIntegration,
     downloadDocument,
+    nameRenameConfigData,
+    setNameRenameConfigData,
+    saveConfigData,
+    datasetForPrePupulatingRename,
   } = props;
   const history = useHistory();
   const theme = useTheme();
@@ -52,7 +62,21 @@ const Preview = (props) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
   const [open, setOpen] = React.useState(false);
   const id = "delete-popper";
-
+  // statusOfModal={statusOfModal}
+  // handleOk={handleOk}
+  // handleCancel={handleCancel}
+  const [statusOfModal, setStatusOfModal] = useState(false);
+  const handleOk = () => {
+    saveConfigData();
+    setStatusOfModal(false);
+  };
+  const handleCancel = () => {
+    setStatusOfModal(false);
+  };
+  const handleOkForSecondButton = () => {
+    downloadDocument();
+    setStatusOfModal(false);
+  };
   const handleDeletePopper = (event) => {
     setAnchorEl(event.currentTarget);
     setOpen(true);
@@ -74,7 +98,13 @@ const Preview = (props) => {
       let val = [];
 
       for (let key in finalDatasetAfterIntegration[0]) {
-        let obj = { field: key, headerName: key, width: 300 };
+        let obj = {
+          field: key,
+          headerName: key,
+          width: 300,
+          renamed_to: "",
+          selectedForExport: true,
+        };
         val.push(obj);
       }
       let rowArr = [];
@@ -229,7 +259,7 @@ const Preview = (props) => {
               styles.flexForBtn
             }
             sx={{ width: "300px !important", height: "182px !important" }}
-            onClick={() => downloadDocument()}
+            onClick={() => setStatusOfModal(true)}
           >
             <img
               src={download_data}
@@ -238,6 +268,29 @@ const Preview = (props) => {
             />{" "}
             <span className="mt-20">Download CSV file</span>
           </Button>
+          {/* <button onClick={() => setStatusOfModal(true)}>Open </button> */}
+          {statusOfModal && (
+            <ControllerModal
+              statusOfModal={statusOfModal}
+              handleOk={() => handleOk()}
+              handleCancel={handleCancel}
+              handleOkForSecondButton={handleOkForSecondButton}
+              modalBody={
+                <ModalBody
+                  col={col}
+                  row={row}
+                  nameRenameConfigData={nameRenameConfigData}
+                  setNameRenameConfigData={setNameRenameConfigData}
+                  setIsConditionForConnectorDataForSaveMet={
+                    setIsConditionForConnectorDataForSaveMet
+                  }
+                  connectorData={connectorData}
+                  saveConfigData={saveConfigData}
+                  datasetForPrePupulatingRename={datasetForPrePupulatingRename}
+                />
+              }
+            />
+          )}
           {/* </div> */}
           {/* </Affix> */}
         </div>
@@ -258,10 +311,10 @@ const Preview = (props) => {
           <Button
             id="cancel-button"
             onClick={() => {
-              history.push("/datahub/connectors");
+              history.push(`/${findType()}/connectors`);
               resetAll(true, true, true, true, setCol, setRow);
             }}
-            className={styles.cancelBtn}
+            className={globalStyle.secondary_button}
           >
             Cancel
           </Button>
