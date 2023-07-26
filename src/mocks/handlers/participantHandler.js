eb53f819c27a8e50ee37be648ea06f2d194e8bae
ShruthiMonika <shruthimonika@ShruthinikasAir.Dlink>
2023-07-26 05:42:04 +0530

    1804

--- src/mocks/handlers/participantHandler.js
+++ src/mocks/handlers/participantHandler.js
@@ -8,11 +8,172 @@ export const participantHandler = [
       (req, res, ctx) => {
         return res(ctx.status(201), ctx.json(
           {
-            id: "a2b12bae-389c-4d56-b11a-7233fa466050",
-            created_at: "2023-07-24T19:12:41.117381Z",
-            updated_at: "2023-07-24T19:12:41.117409Z",
-            user: "f8c58cf7-0523-4cc3-ad34-ae999b4de99b",
-            organization: "bb92b790-e810-462d-b5db-c40139ffa8fe"
+            "id": "a2b12bae-389c-4d56-b11a-7233fa466050",
+            "created_at": "2023-07-24T19:12:41.117381Z",
+            "updated_at": "2023-07-24T19:12:41.117409Z",
+            "user": "f8c58cf7-0523-4cc3-ad34-ae999b4de99b",
+            "organization": "bb92b790-e810-462d-b5db-c40139ffa8fe"
+        }
+        ));
+      }
+    ),
+    rest.post(
+      UrlConstant.base_url + "datahub/participant/get_list_co_steward/",
+      (req, res, ctx) => {
+        return res(ctx.status(201), ctx.json(
+          [
+            {
+                "user": "26192c83-4cd2-4926-b0e5-c77ea58008d0",
+                "organization_name": "Jai's Demo Organization_1"
+            },
+            {
+                "user": "686192d2-d3cc-4dfa-aefe-0f9848935e01",
+                "organization_name": "JSN"
+            },
+            {
+                "user": "fe79f0f3-5c7a-48be-abfc-4a86b6199515",
+                "organization_name": "kanhaiya"
+            },
+            {
+                "user": "65e08d53-5c47-4854-bd3f-a57e4e7ad691",
+                "organization_name": "Kanhaiya"
+            },
+            {
+                "user": "17e2bd9e-cfed-4f40-b788-063784fc7cda",
+                "organization_name": "Kanhaiya Participant Org"
+            },
+            {
+                "user": "2fb4be13-a6c8-4f95-8f78-c45320005180",
+                "organization_name": "monika org costeward"
+            },
+            {
+                "user": "4131d9e6-bdae-47cb-9ad9-01fe6690095e",
+                "organization_name": "nilesh+10@digitalgreen.org"
+            },
+            {
+                "user": "81931fc3-ecc8-4d76-b7b2-f8f2a77dc713",
+                "organization_name": "nilesh+12@digitalgreen.org"
+            },
+            {
+                "user": "3e85eef0-9fb9-491f-9bb7-658bb1aea089",
+                "organization_name": "NS"
+            },
+            {
+                "user": "3ddd3e85-c0f5-44d7-a151-f282702ff7ca",
+                "organization_name": "org"
+            },
+            {
+                "user": "7d23cedc-c205-4b3b-978d-a6a18b9cd582",
+                "organization_name": "sdfghj"
+            },
+            {
+                "user": "56695d23-37f2-4cc1-9abb-9051f7a84761",
+                "organization_name": "shru costeward org and something new to check the space"
+            },
+            {
+                "user": "bed3b6e7-ffec-4460-ae3c-8dd5c85a806a",
+                "organization_name": "Test 2"
+            },
+            {
+                "user": "7d8bf883-a027-4b4a-acd4-771955464e54",
+                "organization_name": "Titan"
+            },
+            {
+                "user": "054d6fc9-cc55-4b8a-85e2-6456fb6bcd14",
+                "organization_name": null
+            }
+        ]
+        ));
+      }
+    ),
+    rest.get(
+      UrlConstant.base_url + UrlConstant.participant + ":id" + "/",
+      (req, res, ctx) => {
+        return res(ctx.status(201), ctx.json(
+          {
+            "id": "d0bb3072-4f42-4e72-835f-9416e1df1ec2",
+            "user_id": "dccf135e-cdc3-4412-aa79-a89db0dfa6bc",
+            "organization_id": "a4e60876-a249-4c45-b13a-7993c2572e27",
+            "user": {
+                "id": "dccf135e-cdc3-4412-aa79-a89db0dfa6bc",
+                "email": "ekta+part@digitalgreen.org",
+                "first_name": "ekta",
+                "last_name": "part",
+                "phone_number": "+91 96114-57777",
+                "role": 3,
+                "status": true,
+                "subscription": null,
+                "profile_picture": null,
+                "on_boarded": true,
+                "on_boarded_by": null,
+                "approval_status": true
+            },
+            "organization": {
+                "id": "a4e60876-a249-4c45-b13a-7993c2572e27",
+                "name": "ekta dummy",
+                "org_email": "ekta+part@digitalgreen.org",
+                "address": {
+                    "city": "",
+                    "address": "patna",
+                    "country": "India",
+                    "pincode": "800001"
+                },
+                "phone_number": "+91 96114-57777",
+                "logo": "/media/organizations/logos/download_y5chEtC.png",
+                "hero_image": null,
+                "org_description": "dhgdhh",
+                "website": "https://www.google.com",
+                "status": true
+            },
+            "dataset_count": 0,
+            "connector_count": 0,
+            "number_of_participants": 0
+        }
+        ));
+      }
+    ),
+    rest.put(
+      UrlConstant.base_url + UrlConstant.participant + ":id" + "/",
+      (req, res, ctx) => {
+        return res(ctx.status(201), ctx.json(
+          {
+            "id": "d0bb3072-4f42-4e72-835f-9416e1df1ec2",
+            "user_id": "dccf135e-cdc3-4412-aa79-a89db0dfa6bc",
+            "organization_id": "a4e60876-a249-4c45-b13a-7993c2572e27",
+            "user": {
+                "id": "dccf135e-cdc3-4412-aa79-a89db0dfa6bc",
+                "email": "ekta+part@digitalgreen.org",
+                "first_name": "ektaaa",
+                "last_name": "part",
+                "phone_number": "+91 96114-57777",
+                "role": 3,
+                "status": true,
+                "subscription": null,
+                "profile_picture": null,
+                "on_boarded": true,
+                "on_boarded_by": null,
+                "approval_status": true
+            },
+            "organization": {
+                "id": "a4e60876-a249-4c45-b13a-7993c2572e27",
+                "name": "ekta dummy",
+                "org_email": "ekta+part@digitalgreen.org",
+                "address": {
+                    "city": "",
+                    "address": "patna",
+                    "country": "India",
+                    "pincode": "800001"
+                },
+                "phone_number": "+91 96114-57777",
+                "logo": "/media/organizations/logos/download_y5chEtC.png",
+                "hero_image": null,
+                "org_description": "dhgdhh",
+                "website": "https://www.google.com",
+                "status": true
+            },
+            "dataset_count": 0,
+            "connector_count": 0,
+            "number_of_participants": 0
         }
         ));
       }
--- src/tests/participants_tests/selfParticipant.test.jsx
+++ src/tests/participants_tests/selfParticipant.test.jsx
@@ -9,7 +9,6 @@ import {
 } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import "@testing-library/jest-dom/extend-expect";
-import userEvent from "@testing-library/user-event";
 import FarmStackProvider from "../../Components/Contexts/FarmStackContext";
 import { BrowserRouter as Router } from "react-router-dom";
 import { setUserMapId } from "../../Utils/Common";
@@ -19,123 +18,207 @@ import ParticipantFormNew from "../../Components/Card/ParticipantForm/Participan
 import UrlConstant from "../../Constants/UrlConstants";
 
 describe("Self register module", () => {
-    beforeEach(() => {
-        cleanup();
-      });
-      afterEach(() => {
-        cleanup();
-      });
-      test("renders Add Support component without crashing", async() => {
-        render(
-          <Router>
-            <ParticipantFormNew />
-          </Router>,
-          {
-            wrapper: FarmStackProvider,
-          }
-        );
-        const orgName = screen.getByLabelText(/organisation name/i); 
-        fireEvent.change(orgName, { target: { value: "DG ORG" } });
+  beforeEach(() => {
+    cleanup();
+  });
+  afterEach(() => {
+    cleanup();
+  });
+
+  test("renders Add participant component event triggering", async () => {
+    render(
+      <Router>
+        <ParticipantFormNew />
+      </Router>,
+      {
+        wrapper: FarmStackProvider,
+      }
+    );
+    const orgName = screen.getByLabelText(/organisation name/i);
+    fireEvent.change(orgName, { target: { value: "DG ORG" } });
+
+    const orgWebsite = screen.getByLabelText(/Website Link/i);
+    fireEvent.change(orgWebsite, {
+      target: { value: "https://www.digitalgreen.org" },
+    });
+
+    const orgAddress = screen.getByLabelText(/Organisation Address /i);
+    fireEvent.change(orgAddress, { target: { value: "bangalore" } });
+
+    const country = screen.getByLabelText(/Country/i);
+    fireEvent.change(country, { target: { value: "India" } });
+
+    const pincode = screen.getByLabelText(/PIN Code/i);
+    fireEvent.change(pincode, { target: { value: "608001" } });
+
+    const firstName = screen.getByLabelText(/First Name/i);
+    fireEvent.change(firstName, { target: { value: "John" } });
+
+    const lastName = screen.getByLabelText(/Last Name/i);
+    fireEvent.change(lastName, { target: { value: "David" } });
+
+    const contactNumber = screen.getByLabelText(/Contact Number /i);
+    fireEvent.change(contactNumber, { target: { value: "9344957735" } });
+  });
+  test("get all data", async () => {
+    render(
+      <Router>
+        <ParticipantFormNew />
+      </Router>,
+      {
+        wrapper: FarmStackProvider,
+      }
+    );
+    const orgName = screen.getByLabelText(/organisation name/i);
+    fireEvent.change(orgName, { target: { value: "DG ORG" } });
+
+    const orgWebsite = screen.getByLabelText(/Website Link/i);
+    fireEvent.change(orgWebsite, {
+      target: { value: "https://www.digitalgreen.org" },
+    });
+
+    const orgAddress = screen.getByLabelText(/Organisation Address /i);
+    fireEvent.change(orgAddress, { target: { value: "bangalore" } });
+
+    const country = screen.getByLabelText(/Country/i);
+    fireEvent.change(country, { target: { value: "India" } });
+
+    const pincode = screen.getByLabelText(/PIN Code/i);
+    fireEvent.change(pincode, { target: { value: "608001" } });
+
+    const firstName = screen.getByLabelText(/First Name/i);
+    fireEvent.change(firstName, { target: { value: "John" } });
+
+    const lastName = screen.getByLabelText(/Last Name/i);
+    fireEvent.change(lastName, { target: { value: "David" } });
+
+    const contactNumber = screen.getByLabelText(/Contact Number /i);
+    fireEvent.change(contactNumber, { target: { value: "9344957735" } });
+
+    const SubmitButton = screen.getByRole("button", {
+      name: /Submit/i,
+    });
+    fireEvent.click(SubmitButton);
+  });
+
+
+  test("click cancel button", async() => {
     
-        // let emailId = await screen.findByRole("textbox", {
-        //   name: /Mail Id/i,
-        // });
-        // expect(emailId.value).toBe("dgemail@digitalgreen.org");
-        // const orgMail = screen.getByLabelText(/Mail Id/i); 
-        // fireEvent.change(orgMail, { target: { value: "shruthi@dg.org" } });
-        const orgWebsite = screen.getByLabelText(/Website Link/i); 
-        fireEvent.change(orgWebsite, { target: { value: "https://www.digitalgreen.org" } });
-
-        const orgAddress = screen.getByLabelText(/Organisation Address /i); 
-        fireEvent.change(orgAddress, { target: { value: "bangalore" } });
-
-        const country = screen.getByLabelText(/Country/i); 
-        fireEvent.change(country, { target: { value: "India" } });
-
-        const pincode = screen.getByLabelText(/PIN Code/i); 
-        fireEvent.change(pincode, { target: { value: "608001" } });
-
-        const firstName = screen.getByLabelText(/First Name/i); 
-        fireEvent.change(firstName, { target: { value: "John" } });
-
-        const lastName = screen.getByLabelText(/Last Name/i); 
-        fireEvent.change(lastName, { target: { value: "David" } });
-
-        const contactNumber = screen.getByLabelText(/Contact Number /i); 
-        fireEvent.change(contactNumber, { target: { value: "9344957735" } });
-
-        const SubmitButton = screen.getByRole("button", {
-          name: /Submit/i,
-        })
-        fireEvent.click(SubmitButton)
-        const cancelButton = screen.getByRole("button", {
-          name: /Cancel/i,
-        })
-        fireEvent.click(cancelButton)
-      });
-
-      test("check for self part button failure", async() => {
-        server.use(
-          rest.post(
-            `${UrlConstant.base_url + UrlConstant.register_participant}`,
-            (req, res, ctx) => {
-              return res(
-                ctx.status(400),
-                ctx.json({
-                  first_name: ["Invalid name"],
-                  name: ["Name already exist"],
-                  website: ["Invalid website link"],
-                  selectedCosteward: ["Invalid"],
-                })
-              );
-            }
-          )
-        );
-        render(
-          <Router>
-            <ParticipantFormNew />
-          </Router>,
-          {
-            wrapper: FarmStackProvider,
-          }
-        );
-        const orgName = screen.getByLabelText(/organisation name/i); 
-        fireEvent.change(orgName, { target: { value: "DG ORG" } });
-    
-        // let emailId = await screen.findByRole("textbox", {
-        //   name: /Mail Id/i,
-        // });
-        // expect(emailId.value).toBe("dgemail@digitalgreen.org");
-        // const orgMail = screen.getByLabelText(/Mail Id/i); 
-        // fireEvent.change(orgMail, { target: { value: "shruthi@dg.org" } });
-        const orgWebsite = screen.getByLabelText(/Website Link/i); 
-        fireEvent.change(orgWebsite, { target: { value: "https://www.digitalgreen.org" } });
-
-        const orgAddress = screen.getByLabelText(/Organisation Address /i); 
-        fireEvent.change(orgAddress, { target: { value: "bangalore" } });
-
-        const country = screen.getByLabelText(/Country/i); 
-        fireEvent.change(country, { target: { value: "India" } });
-
-        const pincode = screen.getByLabelText(/PIN Code/i); 
-        fireEvent.change(pincode, { target: { value: "608001" } });
-
-        const firstName = screen.getByLabelText(/First Name/i); 
-        fireEvent.change(firstName, { target: { value: "John" } });
-
-        const lastName = screen.getByLabelText(/Last Name/i); 
-        fireEvent.change(lastName, { target: { value: "David" } });
-
-        const contactNumber = screen.getByLabelText(/Contact Number /i); 
-        fireEvent.change(contactNumber, { target: { value: "9344957735" } });
-
-        const SubmitButton = screen.getByRole("button", {
-          name: /Submit/i,
-        })
-        fireEvent.click(SubmitButton)
-        const cancelButton = screen.getByRole("button", {
-          name: /Cancel/i,
-        })
-        fireEvent.click(cancelButton)
-      });
-})
+    render(
+      <Router>
+        <ParticipantFormNew />
+      </Router>,
+      {
+        wrapper: FarmStackProvider,
+      }
+    );
+    const cancelButton = screen.getByRole("button", {
+      name: /Cancel/i,
+    });
+    fireEvent.click(cancelButton);
+  })
+  test("check for self part button failure", async () => {
+    server.use(
+      rest.post(
+        `${UrlConstant.base_url + UrlConstant.register_participant}`,
+        (req, res, ctx) => {
+          return res(
+            ctx.status(400),
+            ctx.json({
+              first_name: ["Invalid name"],
+              name: ["Name already exist"],
+              website: ["Invalid website link"],
+              selectedCosteward: ["Invalid"],
+            })
+          );
+        }
+      )
+    );
+    render(
+      <Router>
+        <ParticipantFormNew />
+      </Router>,
+      {
+        wrapper: FarmStackProvider,
+      }
+    );
+  });
+  test("check for get list of costeward", async () => {
+    server.use(
+      rest.post(
+        UrlConstant.base_url + "datahub/participant/get_list_co_steward/",
+        (req, res, ctx) => {
+          return res(
+            ctx.status(400),
+            ctx.json({
+              first_name: ["Invalid name"],
+              name: ["Name already exist"],
+              website: ["Invalid website link"],
+              selectedCosteward: ["Invalid"],
+            })
+          );
+        }
+      )
+    );
+    render(
+      <Router>
+        <ParticipantFormNew />
+      </Router>,
+      {
+        wrapper: FarmStackProvider,
+      }
+    );
+  });
+  test("get details view for edit", async () => {
+    server.use(
+      rest.get(
+        UrlConstant.base_url + UrlConstant.participant + ":id" + "/",
+        (req, res, ctx) => {
+          return res(
+            ctx.status(400),
+            ctx.json({
+              first_name: ["Invalid name"],
+              name: ["Name already exist"],
+              website: ["Invalid website link"],
+              selectedCosteward: ["Invalid"],
+            })
+          );
+        }
+      )
+    );
+    render(
+      <Router>
+        <ParticipantFormNew />
+      </Router>,
+      {
+        wrapper: FarmStackProvider,
+      }
+    );
+  });
+  test("update of part", async () => {
+    server.use(
+      rest.put(
+        UrlConstant.base_url + UrlConstant.participant + ":id" + "/",
+        (req, res, ctx) => {
+          return res(
+            ctx.status(400),
+            ctx.json({
+              first_name: ["Invalid name"],
+              name: ["Name already exist"],
+              website: ["Invalid website link"],
+              selectedCosteward: ["Invalid"],
+            })
+          );
+        }
+      )
+    );
+    render(
+      <Router>
+        <ParticipantFormNew />
+      </Router>,
+      {
+        wrapper: FarmStackProvider,
+      }
+    );
+  });
+});
