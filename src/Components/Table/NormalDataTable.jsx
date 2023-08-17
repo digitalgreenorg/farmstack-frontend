65eefdd990d9f6ac20fb672bd7ea6aa4b7e8879a
kanhaiya suthar <kanhaiyasuthar0@gmail.com>
2023-08-17 15:48:39 +0530

    FSPROD-1913 ADD data table api integration done

--- src/Components/Table/NormalDataTable.jsx
+++ src/Components/Table/NormalDataTable.jsx
@@ -3,6 +3,9 @@ import { Table } from "antd";
 import HTTPService from "../../Services/HTTPService";
 import { Button } from "@mui/material";
 import DownloadIcon from "@mui/icons-material/Download";
+import UrlConstant from "../../Constants/UrlConstants";
+import ReactJson from "react-json-view";
+
 // const columns = [
 //   {
 //     title: "Name",
@@ -32,30 +35,40 @@ import DownloadIcon from "@mui/icons-material/Download";
 //   ...params,
 // });
 
-const NormalDataTable = () => {
+const NormalDataTable = (props) => {
   const [data, setData] = useState();
+  const [my_json_object, setmy_json_object] = useState(null);
+  const [total, setTotal] = useState(50);
   const [loading, setLoading] = useState(false);
   const [tableParams, setTableParams] = useState({
     pagination: {
       current: 1,
-      pageSize: 5,
+      pageSize: 50,
     },
   });
   const [columns, setColumns] = useState([]);
-
   const fetchData = () => {
     setLoading(true);
-    let method = "POST";
+    let method = "GET";
+    let file_path = "";
     let url =
-      "https://datahubethdev.farmstack.co/be/microsite/datasets/dataset_filters/?page=" +
-      tableParams.pagination.current;
+      UrlConstant.base_url +
+      "/microsite/datasets/get_json_response/" +
+      "?page=" +
+      tableParams.pagination.current +
+      "&&file_path=" +
+      props.data[props.selectedFile].file;
 
-    HTTPService(method, url, "", false, false)
+    HTTPService(method, url, "", false, true)
       .then((response) => {
-        setData(response.data.results);
+        // if (tableParams.pagination.current == 1) {
+        //   props.setPreviewForJsonFile(response.data.data);
+        // }
+        setData(response.data.data);
+        // setTotal(response.data.total);
         let cols = [];
         let first = 0;
-        for (let key in response.data.results[0]) {
+        for (let key in response.data.data[0]) {
           let obj = {
             title: key.trim().split("_").join(" "),
             dataIndex: key,
@@ -68,14 +81,13 @@ const NormalDataTable = () => {
           }
           cols.push(obj);
         }
-        console.log(cols);
         setColumns(cols);
         setLoading(false);
         setTableParams({
           ...tableParams,
           pagination: {
             ...tableParams.pagination,
-            total: response.data.count,
+            total: total,
           },
         });
       })
@@ -103,7 +115,7 @@ const NormalDataTable = () => {
   useEffect(() => {
     fetchData();
     console.log("called", tableParams);
-  }, [JSON.stringify(tableParams)]);
+  }, [JSON.stringify(tableParams), props.selectedFile]);
 
   const handleTableChange = (pagination, filters, sorter) => {
     setTableParams({
@@ -119,54 +131,56 @@ const NormalDataTable = () => {
   };
 
   return (
-    <div
-      style={{ width: "90%", margin: "auto" }}
-      className="data_table_inside_dataset_details_view"
-    >
-      <Table
-        title={() => (
-          <div
-            style={{
-              fontFamily: "Montserrat !important",
-              fontWeight: "600",
-              fontSize: "20px",
-              display: "flex",
-              justifyContent: "space-between",
-              alignItems: "center",
-            }}
-          >
-            <div> Farmer profile - Data table</div>
-            <div>
-              {" "}
-              <Button
-                sx={{
-                  border: "1px solid #3366FF",
-                  color: "#3366FF",
-                  textTransform: "capitalize",
-                  size: "20px",
-                }}
-              >
-                <DownloadIcon
-                  fontSize="small"
-                  sx={{ color: "#3366FF !important" }}
-                />{" "}
-                Download
-              </Button>{" "}
+    <>
+      <div
+        style={{ width: "90%", margin: "auto" }}
+        className="data_table_inside_dataset_details_view"
+      >
+        <Table
+          title={() => (
+            <div
+              style={{
+                fontFamily: "Montserrat !important",
+                fontWeight: "600",
+                fontSize: "20px",
+                display: "flex",
+                justifyContent: "space-between",
+                alignItems: "center",
+              }}
+            >
+              <div> Farmer profile - Data table</div>
+              <div>
+                {" "}
+                <Button
+                  sx={{
+                    border: "1px solid #3366FF",
+                    color: "#3366FF",
+                    textTransform: "capitalize",
+                    size: "20px",
+                  }}
+                >
+                  <DownloadIcon
+                    fontSize="small"
+                    sx={{ color: "#3366FF !important" }}
+                  />{" "}
+                  Download
+                </Button>{" "}
+              </div>
             </div>
-          </div>
-        )}
-        columns={columns}
-        rowKey={(record) => record.id}
-        dataSource={data}
-        pagination={tableParams.pagination}
-        loading={loading}
-        onChange={handleTableChange}
-        scroll={{ y: 500, x: 1200 }}
-        bordered={true}
-        rowClassName="row-hover" // Apply the custom row class
-        size="large"
-      />
-    </div>
+          )}
+          columns={columns}
+          rowKey={(record) => record.id}
+          dataSource={data}
+          pagination={tableParams.pagination}
+          loading={loading}
+          onChange={handleTableChange}
+          scroll={{ y: 500, x: 1200 }}
+          bordered={true}
+          rowClassName="row-hover" // Apply the custom row class
+          size="large"
+        />
+      </div>
+    </>
   );
 };
 
