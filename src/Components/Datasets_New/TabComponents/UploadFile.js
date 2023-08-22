bec69d41950959ad00ff9857f21a98be5284cf38
ShruthiMonika <shruthimonika@ShruthiMonikas-MacBook-Air.local>
2023-08-22 11:24:46 +0530

    upload component accordian height adjusted

--- src/Components/Datasets_New/TabComponents/UploadFile.js
+++ src/Components/Datasets_New/TabComponents/UploadFile.js
@@ -1285,7 +1285,7 @@ const UploadFile = ({
                 <></>
               )}
             </Col>
-            <Col className="list_upload_style" style={{height: "500px"}}>
+            <Col className="list_upload_style" >
               <Typography
                 sx={{
                   fontFamily: "Montserrat !important",
