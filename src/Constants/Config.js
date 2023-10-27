e338f87b97ae26c336f2b20ab964ee56a46f4603
kanhaiya suthar <kanhaiyasuthar0@gmail.com>
2023-10-27 14:05:26 +0530

    made title change for eadp

--- src/Constants/Config.js
+++ src/Constants/Config.js
@@ -13,6 +13,16 @@ const CONFIG = {
     primary_color: "",
     secondary_color: "",
   },
+  platform: {
+    powered_by: "FarmStack",
+    name: `ATI's Data Sharing Tool (FarmStack)`,
+    //description can be used for the seo content directly
+    description: `Explore ATI's Data Sharing Tool (FarmStack), powered by FarmStack. Collaborate, analyze, and innovate in agriculture.
+    Join the future of Ethiopian agriculture with our Data Sharing Platform, supported by FarmStack. Access, share, and transform agri data today.
+    FarmStack's ATI's Data sharing Tool (FarmStack) - the hub for sharing, discovering, and leveraging agricultural insights and knowledge.
+    Discover the power of data collaboration in Ethiopian agriculture. Join FarmStack's Data Sharing Platform to drive growth and sustainability.
+    Get involved in Ethiopia's agriculture revolution. FarmStack's Data Sharing Platform connects stakeholders for data-driven farming success`,
+  },
 };
 
 export default CONFIG;
