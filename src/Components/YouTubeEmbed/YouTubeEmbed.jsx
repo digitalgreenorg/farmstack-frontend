b843aa528957ef66c367befc1c7a75632c2b7aeb
Sohit <sohit@digitalgreen.org>
2023-12-11 11:29:04 +0530

    (add/feat) : to show google drive video in view content page.

--- src/Components/YouTubeEmbed/YouTubeEmbed.jsx
+++ src/Components/YouTubeEmbed/YouTubeEmbed.jsx
@@ -16,14 +16,37 @@ function extractVideoId(url) {
     return null;
   }
 }
+function transformDriveLink(link) {
+  // Regular expression to check for drive.google.com and replace /view with /preview
+  const regex = /https?:\/\/drive.google.com\/file\/d\/([a-zA-Z0-9_-]+)\/view/;
+
+  // Check if the link matches the regex
+  const match = link.match(regex);
+
+  if (match) {
+    // If it matches, transform /view to /preview
+    const fileId = match[1];
+    const transformedLink = link.replace("/view", "/preview");
+    return transformedLink;
+  } else {
+    // If it doesn't match, return null
+    return null;
+  }
+}
+
 const YouTubeEmbed = ({ embedUrl }) => {
   const videoId = extractVideoId(embedUrl);
+  const transformedLink = transformDriveLink(embedUrl);
   return (
     <div className="video-responsive">
       <iframe
         width="367px"
         height="206px"
-        src={`https://www.youtube.com/embed/${videoId}`}
+        src={
+          transformedLink
+            ? transformedLink
+            : `https://www.youtube.com/embed/${videoId}`
+        }
         title="YouTube video player"
         frameBorder="0"
         allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
