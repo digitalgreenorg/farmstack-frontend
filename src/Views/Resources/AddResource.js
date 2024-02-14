b209a250f3c50b48442596dddb2a35b13fc71351
Sohit <sohit@digitalgreen.org>
2024-02-14 16:34:08 +0530

    (add) : new featuures for file upload support.

--- src/Views/Resources/AddResource.js
+++ src/Views/Resources/AddResource.js
@@ -12,6 +12,8 @@ import {
   InputLabel,
   Checkbox,
   FormControlLabel,
+  Radio,
+  RadioGroup,
 } from "@mui/material";
 import React, { useContext, useState, useEffect } from "react";
 import {
@@ -41,6 +43,7 @@ import { PoweroffOutlined } from "@ant-design/icons";
 import labels from "../../Constants/labels";
 import CheckBoxWithTypo from "../../Components/Datasets_New/TabComponents/CheckBoxWithTypo";
 import style from "./resources.module.css";
+import ApiConfiguration from "../../Components/Datasets_New/TabComponents/ApiConfiguration";
 
 const accordionTitleStyle = {
   fontFamily: "'Arial' !important",
@@ -68,12 +71,14 @@ const AddResource = (props) => {
     padding: "10px",
   };
   const [fileSizeError, setFileSizeError] = useState("");
-  const fileTypes = ["XLS", "XLSX", "CSV", "JPEG", "PNG", "TIFF", "PDF"];
-
+  const fileTypes = ["DOC", "TEXT", "PDF"];
+  const [selectedValue, setSelectedValue] = useState("public");
   const [resourceName, setResourceName] = useState("");
   const [resourceDescription, setResourceDescription] = useState("");
   const [uploadedFiles, setUploadedFiles] = useState([]);
   const [pdfFiles, setPdfFiles] = useState([]);
+  const [apiLinks, setApiLinks] = useState([]);
+  const [websites, setWebsites] = useState([]);
   const [videoFiles, setVideoFiles] = useState([]);
   const [categories, setCategories] = useState([]);
   const [allCategories, setAllCategories] = useState([]);
@@ -83,6 +88,16 @@ const AddResource = (props) => {
   const [userType, setUserType] = useState("");
   const [key, setKey] = useState(0);
   const [file, setFile] = useState();
+
+  // Api configuration
+  const [api, setApi] = useState();
+  const [authType, setAuthType] = useState("");
+  const [authTypes, setAuthTypes] = useState(["NO_AUTH", "API_KEY", "BEARER"]);
+  const [authToken, setAuthToken] = useState();
+  const [authApiKeyName, setAuthApiKeyName] = useState("");
+  const [authApiKeyValue, setAuthApiKeyValue] = useState("");
+  const [exportFileName, setExportFileName] = useState();
+
   //id stored for the add more
   const [tempIdForAddMoreResourceUrl, setTempIdForAddMoreResourceUrl] =
     useState("");
@@ -108,6 +123,10 @@ const AddResource = (props) => {
         parseFloat(element?.file_size / Math.pow(1024, 2)).toFixed(2) * 1;
     });
     return total.toFixed(2);
+  };
+
+  const handleChange = (event) => {
+    setSelectedValue(event.target.value);
   };
 
   const handleDelete = (index, id, filename, type) => {
@@ -241,7 +260,7 @@ const AddResource = (props) => {
           panel: 1,
           title: (
             <>
-              File Upload
+              Uploaded Files
               {uploadedFiles?.length > 0 ? (
                 <span style={{ color: "#ABABAB", marginLeft: "4px" }}>
                   (Total Files: {uploadedFiles?.length} )
@@ -260,7 +279,7 @@ const AddResource = (props) => {
           panel: 2,
           title: (
             <>
-              PDFs
+              Urls
               {pdfFiles?.length > 0 ? (
                 <span style={{ color: "#ABABAB", marginLeft: "4px" }}>
                   (Total Files: {pdfFiles?.length} )
@@ -279,7 +298,7 @@ const AddResource = (props) => {
           panel: 3,
           title: (
             <>
-              Video
+              Youtube Videos
               {videoFiles?.length > 0 ? (
                 <span style={{ color: "#ABABAB", marginLeft: "4px" }}>
                   (Total Files: {videoFiles?.length} )
@@ -293,6 +312,44 @@ const AddResource = (props) => {
             videoFiles?.length > 0
               ? prepareFile(videoFiles, "video_file")
               : [<EmptyFile text={"You have not uploaded any video"} />],
+        },
+        {
+          panel: 4,
+          title: (
+            <>
+              APIs
+              {apiLinks?.length > 0 ? (
+                <span style={{ color: "#ABABAB", marginLeft: "4px" }}>
+                  (Total Files: {apiLinks?.length} )
+                </span>
+              ) : (
+                <></>
+              )}
+            </>
+          ),
+          details:
+            apiLinks?.length > 0
+              ? prepareFile(apiLinks, "api_links")
+              : [<EmptyFile text={"You have not uploaded any api url"} />],
+        },
+        {
+          panel: 5,
+          title: (
+            <>
+              Websites
+              {websites?.length > 0 ? (
+                <span style={{ color: "#ABABAB", marginLeft: "4px" }}>
+                  (Total Files: {websites?.length} )
+                </span>
+              ) : (
+                <></>
+              )}
+            </>
+          ),
+          details:
+            websites?.length > 0
+              ? prepareFile(websites, "api_links")
+              : [<EmptyFile text={"You have not uploaded any website url"} />],
         },
       ];
       return data;
@@ -673,7 +730,60 @@ const AddResource = (props) => {
       return 2;
     } else if (typeSelected === "video") {
       return 3;
+    } else if (typeSelected === "api") {
+      return 4;
+    } else if (typeSelected === "website") {
+      return 5;
     }
+  };
+
+  const handleExport = () => {
+    // if (selectedUploadType === "rest_api") {
+    //   let present = checkFileAlreadyImported(
+    //     selectedUploadType,
+    //     exportFileName,
+    //     restApifiles
+    //   );
+    //   if (present) {
+    //     return;
+    //   }
+    //   let body = {
+    //     dataset: datasetId,
+    //     dataset_name: dataSetName,
+    //     url: api.trim(),
+    //     file_name: exportFileName.trim(),
+    //     source: "live_api",
+    //     auth_type: authType,
+    //   };
+    //   if (authType === "NO_AUTH") {
+    //     // do nothing for now
+    //   } else if (authType === "API_KEY" && authApiKeyName && authApiKeyValue) {
+    //     body["api_key_name"] = authApiKeyName.trim();
+    //     body["api_key_value"] = authApiKeyValue.trim();
+    //   } else if (authType === "BEARER") {
+    //     body["token"] = authToken.trim();
+    //   }
+    //   let accessToken = getTokenLocal() ?? false;
+    //   callLoader(true);
+    //   HTTPService(
+    //     "POST",
+    //     UrlConstant.base_url + UrlConstant.live_api,
+    //     body,
+    //     false,
+    //     true,
+    //     accessToken
+    //   )
+    //     .then((res) => {
+    //       callLoader(false);
+    //       setRestApiFiles([...restApifiles, res.data]);
+    //       setIsApiConnected(true);
+    //     })
+    //     .catch((err) => {
+    //       callLoader(false);
+    //       console.log(err);
+    //       callToast(err.response?.data?.message, "error", true);
+    //     });
+    // }
   };
 
   useEffect(() => {
@@ -852,6 +962,152 @@ const AddResource = (props) => {
           }
         />
       </Box>
+      <Box className="mt-20">
+        <Typography
+          sx={{
+            fontFamily: "Arial !important",
+            fontWeight: "600",
+            fontSize: "32px",
+            lineHeight: "40px",
+            color: "#000000",
+            textAlign: "left",
+          }}
+        >
+          Usage policy{" "}
+          <span
+            style={{
+              color: "red",
+              fontSize: "26px",
+            }}
+          >
+            *
+          </span>
+        </Typography>
+        <Box className="mt-20">
+          <RadioGroup
+            aria-label="radio-buttons"
+            name="radio-buttons"
+            value={selectedValue}
+            onChange={handleChange}
+            sx={{
+              display: "flex",
+              flexDirection: "row",
+              justifyContent: "space-between",
+              rowGap: "15px",
+            }}
+          >
+            <FormControlLabel
+              value="public"
+              control={<Radio />}
+              sx={{
+                boxShadow:
+                  "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
+                marginLeft: "0px",
+                padding: "5px 10px",
+                borderRadius: "8px",
+              }}
+              label={
+                <>
+                  Public{" "}
+                  <span>
+                    <img
+                      style={{ marginLeft: "10px", marginTop: "-1px" }}
+                      src={require("../../Assets/Img/info.svg")}
+                    />
+                    <span
+                      className="mt-10"
+                      style={{
+                        fontFamily: "Arial !important",
+                        fontWeight: "400",
+                        fontSize: "14px",
+                        lineHeight: "14.63px",
+                        color: "#000000",
+                        marginLeft: "6px",
+                        fontStyle: "italic",
+                      }}
+                    >
+                      Anyone can download the resource.
+                    </span>
+                  </span>
+                </>
+              }
+            />
+
+            <FormControlLabel
+              value="registered"
+              control={<Radio />}
+              sx={{
+                boxShadow:
+                  "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
+                marginLeft: "0px",
+                padding: "5px 10px",
+                borderRadius: "8px",
+              }}
+              label={
+                <>
+                  Registered
+                  <span>
+                    <img
+                      style={{ marginLeft: "10px", marginTop: "-1px" }}
+                      src={require("../../Assets/Img/info.svg")}
+                    />
+                    <span
+                      className="mt-10"
+                      style={{
+                        fontFamily: "Arial !important",
+                        fontWeight: "400",
+                        fontSize: "14px",
+                        lineHeight: "14.63px",
+                        color: "#000000",
+                        marginLeft: "6px",
+                        fontStyle: "italic",
+                      }}
+                    >
+                      Only registered users can download our resource.
+                    </span>
+                  </span>
+                </>
+              }
+            />
+            <FormControlLabel
+              value="private"
+              control={<Radio />}
+              sx={{
+                boxShadow:
+                  "rgba(0, 0, 0, 0.12) 0px 1px 3px, rgba(0, 0, 0, 0.24) 0px 1px 2px",
+                marginLeft: "0px",
+                padding: "5px 10px",
+                borderRadius: "8px",
+              }}
+              label={
+                <>
+                  Private
+                  <span>
+                    <img
+                      style={{ marginLeft: "10px", marginTop: "-1px" }}
+                      src={require("../../Assets/Img/info.svg")}
+                    />
+                    <span
+                      className="mt-10"
+                      style={{
+                        fontFamily: "Arial !important",
+                        fontWeight: "400",
+                        fontSize: "14px",
+                        lineHeight: "14.63px",
+                        color: "#000000",
+                        marginLeft: "6px",
+                        fontStyle: "italic",
+                      }}
+                    >
+                      Only guaranteed users can access the resource.
+                    </span>
+                  </span>
+                </>
+              }
+            />
+          </RadioGroup>
+        </Box>
+      </Box>
       <Divider sx={{ border: "1px solid #ABABAB", marginTop: "59px" }} />
       <Box className="bold_title mt-50">
         {Resource} category{" "}
@@ -944,9 +1200,11 @@ const AddResource = (props) => {
                 label="Select Content Type"
                 placeholder="Select Content Type"
               >
-                <MenuItem value="pdf">Pdf</MenuItem>
-                <MenuItem value="video">Video</MenuItem>
-                <MenuItem value="file">File</MenuItem>
+                <MenuItem value="pdf">Url</MenuItem>
+                <MenuItem value="video">Youtube</MenuItem>
+                <MenuItem value="file">Upload</MenuItem>
+                <MenuItem value="api">API</MenuItem>
+                <MenuItem value="website">Website</MenuItem>
               </Select>
             </FormControl>
             {typeSelected == "file" ? (
@@ -967,7 +1225,9 @@ const AddResource = (props) => {
                 />
               </Box>
             ) : null}
-            {typeSelected == "pdf" || typeSelected === "video" ? (
+            {typeSelected == "pdf" ||
+            typeSelected === "video" ||
+            typeSelected === "website" ? (
               <Box className="mt-10">
                 <TextField
                   // fullWidth
@@ -989,13 +1249,17 @@ const AddResource = (props) => {
                   }}
                   placeholder={
                     typeSelected == "pdf"
-                      ? "Enter pdf link here"
-                      : "Enter Video link here"
+                      ? "Enter the link here"
+                      : typeSelected == "video"
+                      ? "Enter youtube link here"
+                      : "Enter webiste link here"
                   }
                   label={
                     typeSelected == "pdf"
-                      ? "Enter pdf link here"
-                      : "Enter Video link here"
+                      ? "Enter the link here"
+                      : typeSelected == "video"
+                      ? "Enter youtube link here"
+                      : "Enter webiste link here"
                   }
                   value={eachFileDetailData.url}
                   required
@@ -1017,7 +1281,7 @@ const AddResource = (props) => {
                   }}
                   id="add-dataset-name"
                 />
-                {typeSelected !== "pdf" && (
+                {/* {typeSelected !== "pdf" && (
                   <TextField
                     id="add-dataset-description"
                     fullWidth
@@ -1055,7 +1319,29 @@ const AddResource = (props) => {
                       }
                     }}
                   />
-                )}
+                )} */}
+              </Box>
+            ) : null}
+            {typeSelected == "api" ? (
+              <Box className="mt-10">
+                <ApiConfiguration
+                  api={api}
+                  setApi={setApi}
+                  authType={authType}
+                  setAuthType={setAuthType}
+                  authTypes={authTypes}
+                  setAuthTypes={setAuthTypes}
+                  authToken={authToken}
+                  setAuthToken={setAuthToken}
+                  authApiKeyName={authApiKeyName}
+                  setAuthApiKeyName={setAuthApiKeyName}
+                  authApiKeyValue={authApiKeyValue}
+                  setAuthApiKeyValue={setAuthApiKeyValue}
+                  exportFileName={exportFileName}
+                  setExportFileName={setExportFileName}
+                  handleExport={handleExport}
+                  validator={false}
+                />
               </Box>
             ) : null}
             <FormControlLabel
