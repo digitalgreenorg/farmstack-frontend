2251ff87ef730c80691cb13a9ab1255d4ff76d8d
Sohit <sohit@digitalgreen.org>
2024-06-25 12:33:15 +0530

    (add) : restored the project's dashboard code from the forked farmstack repo

--- src/features/default/src/Layout/Datahub.js
+++ src/features/default/src/Layout/Datahub.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect, useContext, lazy } from "react";
-import { Switch, Route, useHistory } from "react-router-dom";
+import { Switch, Route, useHistory, useRouteMatch } from "react-router-dom";
 import AddCoSteward from "../Components/CoSteward/AddCoSteward";
 import AddTeamMember from "../Views/Settings/TeamMembers/AddTeamMember";
 import EditTeamMember from "../Views/Settings/TeamMembers/EditTeamMember";
@@ -87,6 +87,7 @@ const EditDataset = lazy(() =>
   import("../Components/Datasets_New/EditDataset")
 );
 const DashboardNew = lazy(() => import("../Views/Dashboard/DashboardNew"));
+const StreamlitFrame = lazy(() => import("../Views/Pages/Dashboard/streamlit"));
 const Support = lazy(() => import("../Components/Support_New/Support"));
 const SupportView = lazy(() => import("../Components/Support_New/SupportView"));
 const AskSupport = lazy(() => import("../Components/Support_New/SupportForm"));
@@ -116,6 +117,7 @@ function Datahub(props) {
   const theme = useTheme();
   const mobile = useMediaQuery(theme.breakpoints.down("sm"));
   const [verifyLocalData, setVerifyLocalData] = useState(false);
+  const footerMatch = useRouteMatch("/datahub/new_dashboard/:name");
   // const { isVerified } = useContext(FarmStackContext);
 
   const history = useHistory();
@@ -300,6 +302,11 @@ function Datahub(props) {
                   component={DashboardNew}
                 />
               )}
+              <Route
+                exact
+                path="/datahub/new_dashboard/:name"
+                component={StreamlitFrame}
+              />
               {isParticipantsEnabled && (
                 <Route
                   exact
@@ -580,7 +587,7 @@ function Datahub(props) {
           {globalConfig?.footer === "DEFAULT" && (
             <DefaultFooter loginType={"admin"} />
           )}
-          {globalConfig?.footer === "EADP" && (
+          {globalConfig?.footer === "EADP" && !footerMatch && (
             <EadpFooter loginType={"admin"} />
           )}
           {globalConfig?.footer === "KADP" && (
---
+++ src/features/default/src/Views/Pages/Dashboard/streamlit/index.js
@@ -0,0 +1,58 @@
+import { Box, CssBaseline } from "@mui/material";
+import { FarmStackContext } from "common/components/context/DefaultContext/FarmstackProvider";
+import UrlConstant from "common/constants/UrlConstants";
+
+import React, { useContext, useEffect, useState } from "react";
+import { useParams } from "react-router-dom";
+
+const StreamlitFrame = () => {
+  const { callLoader } = useContext(FarmStackContext);
+  const { name } = useParams();
+  const [url, setUrl] = useState("");
+
+  useEffect(() => {
+    callLoader(true);
+    fetch(`${UrlConstant.base_url}datahub/streamlit/${name}/`)
+      .then((response) => response.text())
+      .then((html) => {
+        callLoader(false);
+        const parser = new DOMParser();
+        const doc = parser.parseFromString(html, "text/html");
+        const iframeSrc = doc.querySelector("iframe").getAttribute("src");
+        setUrl(iframeSrc);
+      })
+      .catch((error) => {
+        callLoader(false);
+        console.error("Error fetching data:", error);
+      });
+  }, []);
+
+  return (
+    <>
+      <CssBaseline />
+      <Box
+        sx={{
+          width: "100vw",
+          height: "100vh",
+          overflow: "hidden",
+          margin: 0,
+          padding: 0,
+          position: "absolute",
+          top: 0,
+          left: 0,
+          zIndex: 1000,
+        }}
+      >
+        <iframe
+          src={url}
+          width="100%"
+          height="100%"
+          frameBorder="0"
+          allowFullScreen
+        ></iframe>
+      </Box>
+    </>
+  );
+};
+
+export default StreamlitFrame;
